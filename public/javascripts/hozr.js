dc7dbd5e89b734febe7bbdaa7253bd4ea5b8c87c
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-22 14:51:15 +0100

    added hozr javascript

---
+++ public/javascripts/hozr.js
@@ -0,0 +1,7 @@
+function showActions(id) {
+  document.getElementById('actions_' + id).style.display = 'inline'
+}
+
+function hideActions(id) {
+  document.getElementById('actions_' + id).style.display = 'none'
+}
