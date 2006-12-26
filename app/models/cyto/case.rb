98a3a3564533752cfbc2b66217d5f50a455a2bc4
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-26 19:29:46 +0100

    parse date shortcuts for examination_date

--- app/models/cyto/case.rb
+++ app/models/cyto/case.rb
@@ -3,4 +3,12 @@ class Cyto::Case < ActiveRecord::Base
   belongs_to :classification
 
   has_and_belongs_to_many :finding_classes
+
+  def examination_date=(value)
+    day, month, year = value.split('.')
+    month ||= Date.today.month
+    year ||= Date.today.year
+  
+    write_attribute(:examination_date, "#{year}-#{month}-#{day}")
+  end
 end
