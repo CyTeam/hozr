85fa2dee690fd2e4616f30ef94433da6de9efce0
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-26 19:30:11 +0100

    fix finding to case association

--- app/models/cyto/finding_class.rb
+++ app/models/cyto/finding_class.rb
@@ -1,2 +1,3 @@
 class Cyto::FindingClass < ActiveRecord::Base
+  has_and_belongs_to_many :cases
 end
---
+++ db/migrate/018_cases_finding_classes_needs_no_id.rb
@@ -0,0 +1,9 @@
+class CasesFindingClassesNeedsNoId < ActiveRecord::Migration
+  def self.up
+    remove_column(:cases_finding_classes, :id)
+  end
+
+  def self.down
+    raise IrreversibleMigration
+  end
+end
