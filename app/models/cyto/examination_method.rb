2873dd4fa8f6f35847407b61d65c82ae3764dc7b
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-26 13:36:12 +0100

    added model examination_method

---
+++ app/models/cyto/examination_method.rb
@@ -0,0 +1,2 @@
+class Cyto::ExaminationMethod < ActiveRecord::Base
+end
---
+++ db/migrate/013_create_cyto_examination_methods.rb
@@ -0,0 +1,11 @@
+class CreateCytoExaminationMethods < ActiveRecord::Migration
+  def self.up
+    create_table :examination_methods do |t|
+      # t.column :name, :string
+    end
+  end
+
+  def self.down
+    drop_table :examination_methods
+  end
+end
