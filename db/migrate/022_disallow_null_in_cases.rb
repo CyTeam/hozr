6a06df08ee07710970dd838111b0588589aeb63c
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-27 12:00:46 +0100

    disallow null in cases.(doctor_id, patient_id, examination_date, examination_method_id)

---
+++ db/migrate/022_disallow_null_in_cases.rb
@@ -0,0 +1,15 @@
+class DisallowNullInCases < ActiveRecord::Migration
+  def self.up
+    change_column :cases, :doctor_id, :integer, :null => false
+    change_column :cases, :patient_id, :integer, :null => false
+    change_column :cases, :examination_date, :date, :null => false
+    change_column :cases, :examination_method_id, :integer, :null => false
+  end
+
+  def self.down
+    change_column :cases, :doctor_id, :integer, :null => true
+    change_column :cases, :patient_id, :integer, :null => true
+    change_column :cases, :examination_date, :date, :null => true
+    change_column :cases, :examination_method_id, :integer, :null => true
+  end
+end
