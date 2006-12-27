b72d6fd77f09bfaa9f7eff2c9674740f27268e54
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-27 08:09:35 +0100

    parse eingangsnr in deprecated pap form

--- app/models/cyto/case.rb
+++ app/models/cyto/case.rb
@@ -4,6 +4,19 @@ class Cyto::Case < ActiveRecord::Base
 
   has_and_belongs_to_many :finding_classes
 
+  def praxistar_eingangsnr=(value)
+    left, right = value.split('/')
+    if right.nil?
+      year = '06'
+      number = sprintf("%05d", left)
+    else
+      year = sprintf("%02d", left)
+      number = sprintf("%05d", right)
+    end
+  
+    write_attribute(:praxistar_eingangsnr, "#{year}/#{number}")
+  end
+
   def examination_date=(value)
     day, month, year = value.split('.')
     month ||= Date.today.month
