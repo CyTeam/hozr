0da09abe38494acd0d5ad498887e60a5ed794349
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-27 13:23:47 +0100

    fix billing vcard problem with patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
   belongs_to :insurance
   belongs_to :doctor
   belongs_to :vcard
-  belongs_to :billing_vcard, :class_name => 'Vcard'
+  belongs_to :billing_vcard, :class_name => 'Vcard', :foreign_key => 'billing_vcard_id'
 
   has_many :cases
 end
