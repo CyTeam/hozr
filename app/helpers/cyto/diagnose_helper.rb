1283c0882a9f6ccee9d5a2497de0c59c74a26d31
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-27 20:03:42 +0100

    use code to identify classifications in pap_form

--- app/helpers/cyto/diagnose_helper.rb
+++ app/helpers/cyto/diagnose_helper.rb
@@ -1,2 +1,7 @@
 module Cyto::DiagnoseHelper
+  def classification_button(code)
+    classification = Classification.find_by_code(code)
+    
+    button_to classification.name, { :action => "second_entry_form", :classification => classification.id }, :style => "background-color: #{classification.color}"
+  end
 end
