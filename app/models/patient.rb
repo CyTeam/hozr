59f351fbbaaf86346692990b8a72be6fe5d22a37
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-27 11:13:44 +0100

    billing_vcard of patient is of class Vcard

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
   belongs_to :insurance
   belongs_to :doctor
   belongs_to :vcard
-  belongs_to :billing_vcard
+  belongs_to :billing_vcard, :class_name => 'Vcard'
 
   has_many :cases
 end
