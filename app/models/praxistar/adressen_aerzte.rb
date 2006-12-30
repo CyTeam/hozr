96d7ce187458081ee5b32e523d81c7c364341b65
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-30 16:35:26 +0100

    added Praxistar::AdressenAerzte model

---
+++ app/models/praxistar/adressen_aerzte.rb
@@ -0,0 +1,32 @@
+class Praxistar::AdressenAerzte < ActiveRecord::Base
+  set_table_name "Adressen_\304rzte"
+  set_primary_key "ID_Arztadresse"
+
+  establish_connection(
+    :adapter => 'sqlserver',
+    :mode => 'odbc',
+    :dsn => 'praxistar',
+    :username => 'sa',
+    :password => 'pioneer'
+  )
+
+  def self.import
+    for a in find_all
+      Doctor.new(
+        :id => a.ID_Arztadresse,
+        :vcard => Vcard.new(
+          :locality => a.tx_Prax_Ort,
+          :fax_number => a.tx_Prax_Fax,
+          :phone_number => a.tx_Prax_Telefon1,
+          :postal_code => a.tx_Prax_PLZ,
+          :street_address => a.tx_Prax_Strasse,
+          :family_name => a.tx_Name,
+          :given_name => a.tx_Vorname
+        ),
+        :code => a.tx_ErfassungsNr,
+        :speciality => a.tx_Fachgebiet
+      ).save
+    end
+  end
+
+end
---
+++ test/unit/praxistar/adressen_aerzte_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Praxistar::AdressenAerzteTest < Test::Unit::TestCase
+  fixtures :adressen_aerztes
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
