600c4baf048b9c42437aa36f5daf07bdbf9b8931
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-30 16:15:11 +0100

    added import class method to AdressenVersicherungen

--- app/models/praxistar/adressen_versicherungen.rb
+++ app/models/praxistar/adressen_versicherungen.rb
@@ -9,4 +9,19 @@ class Praxistar::AdressenVersicherungen < ActiveRecord::Base
     :username => 'sa',
     :password => 'pioneer'
   )
+
+  def self.import
+    for a in find_all
+      Insurance.new(
+        :id => a.ID_Versicherung,
+        :phone_number => a.tx_Telefon,
+        :locality => a.tx_Ort,
+        :fax_number => a.tx_FAX,
+        :extended_address => a.tx_ZuHanden,
+        :postal_code => a.tx_PLZ,
+        :street_address => a.tx_Strasse,
+        :name => a.tx_Name
+      ).save
+    end
+  end
 end
