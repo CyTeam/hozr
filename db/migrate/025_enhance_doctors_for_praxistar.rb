1ead9651d41cdcc9a8857f62caacaddc49b3795d
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-30 16:35:02 +0100

    added migration script for praxistar importer

---
+++ db/migrate/025_enhance_doctors_for_praxistar.rb
@@ -0,0 +1,11 @@
+class EnhanceDoctorsForPraxistar < ActiveRecord::Migration
+  def self.up
+    add_column :doctors, :code, :string
+    add_column :doctors, :field, :string
+  end
+
+  def self.down
+    remove_column :doctors, :code
+    remove_column :doctors, :field
+  end
+end
---
+++ db/migrate/026_rename_field_to_specialty_in_doctors.rb
@@ -0,0 +1,9 @@
+class RenameFieldToSpecialtyInDoctors < ActiveRecord::Migration
+  def self.up
+    rename_column :doctors, :field, :speciality
+  end
+
+  def self.down
+    rename_column :doctors, :speciality, :field
+  end
+end
