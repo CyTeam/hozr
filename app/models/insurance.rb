51edfb5a99f322d66f2f1170f6d408b48f3f1cba
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-30 13:04:39 +0100

    add convenience accessor methods for vard attributes to insurance model

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,11 +1,106 @@
 class Insurance < ActiveRecord::Base
   belongs_to :vcard
 
+  # Vcard
   def name
-    vcard.family_name
+    vcard.full_name
   end
 
   def name=(name)
-    vcard.family_name = name
+    create_vcard if vcard.nil?
+    vcard.full_name = name
+  end
+
+  def post_office_box
+    vcard.post_office_box
+  end
+
+  def post_office_box=(value)
+    create_vcard if post_office_box.nil?
+    vcard.post_office_box = value
+  end
+
+  def extended_vcard
+    vcard.extended_vcard
+  end
+
+  def extended_vcard=(value)
+    create_vcard if vcard.nil?
+    vcard.extended_vcard = value
+  end
+
+  def street_vcard
+    vcard.street_vcard
+  end
+
+  def street_address=(value)
+    create_address if vcard.nil?
+    vcard.street_address = value
+  end
+
+  def locality
+    vcard.locality
+  end
+
+  def locality=(value)
+    create_vcard  if vcard.nil?
+    vcard.locality = value
+  end
+
+  def region
+    vcard.region
+  end
+
+  def region=(value)
+    create_vcard if vcard.nil?
+    vcard.region = value
+  end
+
+  def postal_code
+    vcard.postal_code
+  end
+
+  def postal_code=(value)
+    create_vcard if vcard.nil?
+    vcard.postal_code = value
+  end
+
+  def country_name
+    vcard.country_name
+  end
+
+  def country_name=(value)
+    create_vcard if vcard.nil?
+    vcard.country_name = value
+  end
+
+  def phone_number=(value)
+    phone_numbers.build(:number => value, :phone_number_type => 'business')
+  end
+
+  def phone_number
+    begin
+      phone_numbers[0].number
+    rescue
+      return ''
+    end
+  end
+
+  def fax_number=(value)
+    phone_numbers.build(:number => value, :phone_number_type => 'fax')
+  end
+
+  def fax_number
+    begin
+      fax_numbers[0].number
+    rescue
+      return ''
+    end
+  end
+
+
+  def save
+    vcard.save
+    super
   end
 end
