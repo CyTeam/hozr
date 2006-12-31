dcfadff38ee516eca00ede0f6e91d46efdeac15a
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-31 01:26:17 +0100

    implemented zuordnungs importer

--- app/models/praxistar/praxilab_zuordnung.rb
+++ app/models/praxistar/praxilab_zuordnung.rb
@@ -1,2 +1,20 @@
-class Praxistar::PraxilabZuordnung < ActiveRecord::Base
+include Cyto
+
+class Praxistar::PraxilabZuordnung < Praxistar::Base
+  set_table_name "Praxilab_Zuordnung"
+
+  def self.import
+    #Classification.delete_all
+
+    for a in find(:all, :limit => 100)
+      print "#{a.Praxilab_ID}\n"
+      
+      begin
+        c = Cyto::Case.find(a.Praxilab_ID)
+        c.finding_classes<< FindingClass.find(a.Diagnosenliste_ID)
+      rescue ActiveRecord::RecordNotFound
+        print " not yet imported\n"
+      end
+    end
+  end
 end
