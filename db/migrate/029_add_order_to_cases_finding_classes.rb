4829a08071bd15f79b09a4b678aef2ab66d40707
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2006-12-31 01:04:04 +0100

    add order to cases finding classes migration

---
+++ db/migrate/029_add_order_to_cases_finding_classes.rb
@@ -0,0 +1,9 @@
+class AddOrderToCasesFindingClasses < ActiveRecord::Migration
+  def self.up
+    add_column :cases_finding_classes, :order, :integer
+  end
+
+  def self.down
+    remove_column :cases_finding_classes, :order
+  end
+end
