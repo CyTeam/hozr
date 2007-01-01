19e8dfcfdfb364bd4aa84a1fb67a9fa3b8d71f2e
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-01 12:55:34 +0100

    import more fields for patients

--- app/models/praxistar/patienten_personalien.rb
+++ app/models/praxistar/patienten_personalien.rb
@@ -16,10 +16,24 @@ class Praxistar::PatientenPersonalien < Praxistar::Base
             :family_name => a.tx_Name,
             :given_name => a.tx_Vorname
           ),
+          :billing_vcard => Vcard.new(
+            :locality => a.tx_fakt_Ort,
+            :postal_code => a.tx_fakt_PLZ,
+            :street_address => a.tx_fakt_Strasse,
+            :family_name => a.tx_fakt_Name,
+            :given_name => a.tx_fakt_Vorname,
+            :extended_address => a.tx_fakt_ZuHanden
+          ),
           :insurance_id => a.KK_Garant_ID,
           :insurance_nr => a.tx_KK_MitgliedNr,
           :doctor_id => a.ZuwArzt_ID,
-          :birth_date => a.tx_Geburtsdatum
+          :birth_date => a.tx_Geburtsdatum,
+          :created_at => a.tx_Aufnahmedatum,
+          :updated_at => a.dt_Mutationsdatum,
+          :remarks => a.mo_Bemerkung,
+          :sex => a.Geschlecht_ID,
+          :dunning_stop => a.tf_Mahnen,
+          :use_billing_address => a.tf_fakt_Aktiv
         )
         p.id = a.ID_Patient
         p.save
