f0b8214161c2866e6de0ed37b88941f07f772ce5
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 20:30:06 +0100

    add_index_for_updated_at_in_patients.rb

---
+++ db/migrate/045_add_index_for_updated_at_in_patients.rb
@@ -0,0 +1,9 @@
+class AddIndexForUpdatedAtInPatients < ActiveRecord::Migration
+  def self.up
+    add_index :patients, :updated_at
+  end
+
+  def self.down
+    remove_index :patients, :updated_at
+  end
+end
