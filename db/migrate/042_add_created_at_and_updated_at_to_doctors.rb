01b7b82ba235187078fb547b3a80b2d225e51fd1
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 18:24:55 +0100

    dd_created_at_and_updated_at_to_doctors

---
+++ db/migrate/042_add_created_at_and_updated_at_to_doctors.rb
@@ -0,0 +1,11 @@
+class AddCreatedAtAndUpdatedAtToDoctors < ActiveRecord::Migration
+  def self.up
+    add_column :doctors, :created_at, :datetime
+    add_column :doctors, :updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :doctors, :created_at
+    remove_column :doctors, :updated_at
+  end
+end
