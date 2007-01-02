80139cd8b07fa39b99357baaf18b628737f79f84
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 01:27:27 +0100

    add insurance to cases

---
+++ db/migrate/035_add_insurance_to_cases.rb
@@ -0,0 +1,11 @@
+class AddInsuranceToCases < ActiveRecord::Migration
+  def self.up
+    add_column :cases, :insurance_id, :integer
+    add_column :cases, :insurance_nr, :string
+  end
+
+  def self.down
+    remove_column :cases, :insurance_id
+    remove_column :cases, :insurance_nr
+  end
+end
