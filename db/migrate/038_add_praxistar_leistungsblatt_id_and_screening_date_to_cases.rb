ff4858f41d16d251bdc74c7d0bfeb9d1b1738918
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 11:04:04 +0100

    add praxistar leistungsblatt id and screening date to cases

---
+++ db/migrate/038_add_praxistar_leistungsblatt_id_and_screening_date_to_cases.rb
@@ -0,0 +1,11 @@
+class AddPraxistarLeistungsblattIdAndScreeningDateToCases < ActiveRecord::Migration
+  def self.up
+    add_column :cases, :praxistar_leistungsblatt_id, :integer
+    add_column :cases, :screened_at, :datetime
+  end
+
+  def self.down
+    remove_column :cases, :praxistar_leistungsblatt_id
+    remove_column :cases, :screened_at
+  end
+end
