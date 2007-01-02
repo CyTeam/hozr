b457ac2fed817c696d13dfbb4123db3c7fda4fe6
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 18:25:17 +0100

    add_index_for_vcard_ids

---
+++ db/migrate/044_add_index_for_vcard_ids.rb
@@ -0,0 +1,11 @@
+class AddIndexForVcardIds < ActiveRecord::Migration
+  def self.up
+    add_index :addresses, :vcard_id
+    add_index :phone_numbers, :vcard_id
+  end
+
+  def self.down
+    remove_index :addresses, :vcard_id
+    remove_index :phone_numbers, :vcard_id
+  end
+end
