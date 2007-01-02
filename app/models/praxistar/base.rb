820312ae519c33b52f7765129219f41fa3f4fbcd
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 18:23:59 +0100

    move export functionality to Praxistar::Base and use it in Praxistar::AdressenAerzte

--- app/models/praxistar/base.rb
+++ app/models/praxistar/base.rb
@@ -4,4 +4,32 @@ class Praxistar::Base < ActiveRecord::Base
   praxistar_connection = YAML.load(File.open(File.join(RAILS_ROOT,"config/database.yml"),"r"))["praxistar_"+ ENV['RAILS_ENV']]
   
   establish_connection(praxistar_connection)
+
+  def self.export
+    last_export = Exports.find(:first, :conditions => "model = '#{self.name}'", :order => "finished_at DESC")
+    
+    find_params = {
+      :conditions => [ "updated_at >= ?", last_export.started_at ]
+    } unless last_export.nil?
+    
+    export = Exports.new(:started_at => Time.now, :find_params => find_params, :model => self.name)
+
+    for h in hozr_model.find(:all, find_params)
+      attributes = export_attributes(h)
+      if exists?(h.id)
+        update(h.id, attributes)
+        export.update_count += 1
+      else
+        p = new(attributes)
+        p.id = h.id
+        p.save
+        export.create_count += 1
+      end
+    end
+  
+    export.finished_at = Time.now
+    export.save
+  
+    print export.to_s
+  end
 end
