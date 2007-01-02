fe62c016b297fd47d9616fc7ac624a06938e0f28
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-02 18:43:05 +0100

    use generic export method in AdressenVersicherungen

--- app/models/praxistar/adressen_versicherungen.rb
+++ app/models/praxistar/adressen_versicherungen.rb
@@ -2,6 +2,10 @@ class Praxistar::AdressenVersicherungen < Praxistar::Base
   set_table_name "Adressen_Versicherungen"
   set_primary_key "ID_Versicherung"
 
+  def self.hozr_model
+    Insurance
+  end
+
   def self.import
     Insurance.delete_all
 
@@ -20,36 +24,15 @@ class Praxistar::AdressenVersicherungen < Praxistar::Base
     end
   end
 
-  def self.export
-    last_export = Exports.find(:first, :conditions => "model = '#{self.name}'", :order => "finished_at DESC")
-    
-    find_params = {
-      :conditions => [ "updated_at >= ?", last_export.started_at ]
+  def self.export_attributes(hozr_record)
+    {
+      :tx_Telefon => hozr_record.phone_number,
+      :tx_Ort => hozr_record.locality,
+      :tx_FAX => hozr_record.fax_number,
+      :tx_ZuHanden => hozr_record.extended_address,
+      :tx_PLZ => hozr_record.postal_code,
+      :tx_Strasse => hozr_record.street_address,
+      :tx_Name => hozr_record.name
     }
-    export = Exports.new(:started_at => Time.now, :find_params => find_params, :model => self.name)
-
-    for h in Insurance.find(:all, find_params)
-      attributes = {
-        :tx_Telefon => h.phone_number,
-        :tx_Ort => h.locality,
-        :tx_FAX => h.fax_number,
-        :tx_ZuHanden => h.extended_address,
-        :tx_PLZ => h.postal_code,
-        :tx_Strasse => h.street_address,
-        :tx_Name => h.name
-      }
-      if exists?(h.id)
-        update(h.id, attributes)
-        export.update_count += 1
-      else
-        p = new(attributes)
-        p.id = h.id
-        p.save
-        export.create_count += 1
-      end
-    end
-  
-    export.finished_at = Time.now
-    export.save
   end
 end
