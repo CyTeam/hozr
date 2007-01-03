14b318edc83145ea520dc5a1d8b345e638a15be5
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-03 14:31:35 +0100

    log exceptions

--- app/models/praxistar/praxilab_zuordnung.rb
+++ app/models/praxistar/praxilab_zuordnung.rb
@@ -11,11 +11,13 @@ class Praxistar::PraxilabZuordnung < Praxistar::Base
         c = Cyto::Case.find(a.Praxilab_ID)
         c.finding_classes<< FindingClass.find(a.Diagnosenliste_ID)
       rescue ActiveRecord::RecordNotFound
+        c.logger.info "ID: #{a.Praxilab_ID} => not yet imported\n"
         print "ID: #{a.Praxilab_ID} => not yet imported\n"
       rescue Exception => ex
         print "ID: #{a.Praxilab_ID}, Diagnose: #{a.Diagnosenliste_ID} => #{ex.message}\n\n"
-        print ex.backtrace.join("\n\t")
-        print "\n\n"
+        c.logger.info "ID: #{a.Praxilab_ID}, Diagnose: #{a.Diagnosenliste_ID} => #{ex.message}\n\n"
+        c.logger.info ex.backtrace.join("\n\t")
+        c.logger.info "\n\n"
       end
     end
   end
