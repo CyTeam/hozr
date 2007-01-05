0aa87ab2fcdfc1fbe730c66d5b8d94b6770d760c
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-05 12:09:26 +0100

    allow humanized for for birth_date

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -10,4 +10,17 @@ class Patient < ActiveRecord::Base
   def name
     vcard.full_name unless vcard.nil?
   end
+
+  def birth_date=(value)
+    if value.is_a?(String)
+      day, month, year = value.split('.')
+      month ||= Date.today.month
+      year ||= Date.today.year
+      year = 1900 + year.to_i if year.to_i < 100
+      
+      write_attribute(:birth_date, "#{year}-#{month}-#{day}")
+    else
+      write_attribute(:birth_date, value)
+    end
+  end
 end
