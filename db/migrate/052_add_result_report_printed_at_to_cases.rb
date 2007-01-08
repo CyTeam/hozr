2f11fa0993dc08ff4de02d40dd55d810bff29aad
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-08 08:45:42 +0100

    dd_result_report_printed_at_to_cases

---
+++ db/migrate/052_add_result_report_printed_at_to_cases.rb
@@ -0,0 +1,9 @@
+class AddResultReportPrintedAtToCases < ActiveRecord::Migration
+  def self.up
+    add_column :cases, :result_report_printed_at, :datetime
+  end
+
+  def self.down
+    remove_column :cases, :result_report_printed_at
+  end
+end
