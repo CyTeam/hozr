6403a1152e92647592a38f30df2a11fd92fc5635
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-11 21:48:49 +0100

    tarmed_leistungen of case shall return a leistung, no zuordnung

--- app/models/cyto/classification.rb
+++ app/models/cyto/classification.rb
@@ -1,12 +1,12 @@
 include Cyto
 
 class Cyto::Classification < ActiveRecord::Base
-  has_many :classification_tarmed_leistungen
-  has_many :tarmed_leistungen, :class_name => 'ClassificationTarmedLeistungen', :order => 'position'
+  has_many :classification_tarmed_leistungen, :order => 'position'
+  has_many :tarmed_leistungen, :class_name => 'ClassificationTarmedLeistungen'
   belongs_to :examination_method
   has_and_belongs_to_many :top_finding_classes, :join_table => 'top_finding_classes', :class_name => 'FindingClass', :foreign_key => 'classification_id'
 
-#  def tarmed_leistungen
-#    classification_tarmed_leistungen.collect { |l| l.tarmed_leistung }
-#  end
+  def tarmed_leistungen
+    classification_tarmed_leistungen.collect { |l| l.tarmed_leistung }
+  end
 end
