e06e45b404e888be317c062fb58fdc7aff3fa468
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-11 19:33:36 +0100

    tarmed_leistungen association for classification is ordered by position

--- app/models/cyto/classification.rb
+++ app/models/cyto/classification.rb
@@ -2,7 +2,7 @@ include Cyto
 
 class Cyto::Classification < ActiveRecord::Base
   has_many :classification_tarmed_leistungen
-  has_many :tarmed_leistungen, :class_name => 'ClassificationTarmedLeistungen'
+  has_many :tarmed_leistungen, :class_name => 'ClassificationTarmedLeistungen', :order => 'position'
   belongs_to :examination_method
   has_and_belongs_to_many :top_finding_classes, :join_table => 'top_finding_classes', :class_name => 'FindingClass', :foreign_key => 'classification_id'
 
