4a45eef7046549c43c1369dc0c8366727b8b1a77
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-11 07:25:16 +0100

    add methods to get HTTP authorization to application controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -91,4 +91,33 @@ end
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  private
+  def get_auth_data 
+    user, pass = '', '' 
+    # extract authorisation credentials 
+    if request.env.has_key? 'X-HTTP_AUTHORIZATION' 
+      # try to get it where mod_rewrite might have put it 
+      authdata = request.env['X-HTTP_AUTHORIZATION'].to_s.split 
+    elsif request.env.has_key? 'Authorization' 
+      # for Apace/mod_fastcgi with -pass-header Authorization 
+      authdata = request.env['Authorization'].to_s.split 
+    elsif request.env.has_key? 'HTTP_AUTHORIZATION' 
+      # this is the regular location 
+      authdata = request.env['HTTP_AUTHORIZATION'].to_s.split  
+    elsif request.env.has_key? 'Authorization'
+      # this is the regular location, for Apache 2
+      authdata = @request.env['Authorization'].to_s.split
+    end 
+
+    # at the moment we only support basic authentication 
+    if authdata and authdata[0] == 'Basic' 
+      user, pass = Base64.decode64(authdata[1]).split(':')[0..1] 
+    end 
+    return [user, pass]
+  end
+
+  def get_user
+    user, pass = get_auth_data
+    return user
+  end
 end
