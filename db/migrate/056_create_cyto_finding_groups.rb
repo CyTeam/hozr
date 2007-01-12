894eef500c489f78b6e3261326afe907772863f1
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-12 08:46:49 +0100

    implemented finding groups

---
+++ db/migrate/056_create_cyto_finding_groups.rb
@@ -0,0 +1,11 @@
+class CreateCytoFindingGroups < ActiveRecord::Migration
+  def self.up
+    create_table :finding_groups do |t|
+      t.column :name, :string
+    end
+  end
+
+  def self.down
+    drop_table :finding_groups
+  end
+end
---
+++ db/migrate/057_create_cyto_finding_classes_finding_groups.rb
@@ -0,0 +1,12 @@
+class CreateCytoFindingClassesFindingGroups < ActiveRecord::Migration
+  def self.up
+    create_table :finding_classes_finding_groups, :id => false do |t|
+      t.column :finding_class_id, :integer
+      t.column :finding_group_id, :integer
+    end
+  end
+
+  def self.down
+    drop_table :finding_classes_finding_groups
+  end
+end
