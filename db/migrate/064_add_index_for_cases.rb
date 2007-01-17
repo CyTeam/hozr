b0e681099bca259d313f11ef650d85268672a08b
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-17 23:42:56 +0100

    add_index_for_cases

---
+++ db/migrate/064_add_index_for_cases.rb
@@ -0,0 +1,13 @@
+class AddIndexForCases < ActiveRecord::Migration
+  def self.up
+    add_index :cases, :patient_id
+    add_index :cases, :doctor_id
+    add_index :cases, :insurance_id
+  end
+
+  def self.down
+    remove_index :cases, :patient_id
+    remove_index :cases, :doctor_id
+    remove_index :cases, :insurance_id
+  end
+end
