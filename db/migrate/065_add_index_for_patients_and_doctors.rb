f13890ae2f8db75f35044758fcaf59b2a2957462
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-01-17 23:52:36 +0100

    add_index_for_patients_and_doctors

---
+++ db/migrate/065_add_index_for_patients_and_doctors.rb
@@ -0,0 +1,19 @@
+class AddIndexForPatientsAndDoctors < ActiveRecord::Migration
+  def self.up
+    add_index :patients, :vcard_id
+    add_index :patients, :billing_vcard_id
+    add_index :patients, :doctor_id
+    add_index :patients, :insurance_id
+    add_index :doctors, :praxis_vcard
+    add_index :doctors, :private_vcard
+  end
+
+  def self.down
+    remove_index :patients, :vcard_id
+    remove_index :patients, :billing_vcard_id
+    remove_index :patients, :doctor_id
+    remove_index :patients, :insurance_id
+    remove_index :doctors, :praxis_vcard
+    remove_index :doctors, :private_vcard
+  end
+end
