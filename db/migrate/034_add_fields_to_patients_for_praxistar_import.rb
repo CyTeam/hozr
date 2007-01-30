b88db0527e2b845d2d5bcc91c033d02d304b1804
Ramon Egloff <ramon.egloff@zyto-labor.com>
2007-01-30 07:06:34 +0100

    db_migrate cleanup (remove duplicate remarks)

--- db/migrate/034_add_fields_to_patients_for_praxistar_import.rb
+++ db/migrate/034_add_fields_to_patients_for_praxistar_import.rb
@@ -2,7 +2,6 @@ class AddFieldsToPatientsForPraxistarImport < ActiveRecord::Migration
   def self.up
     add_column :patients, :created_at, :datetime
     add_column :patients, :updated_at, :datetime
-    add_column :patients, :remarks, :string
     add_column :patients, :dunning_stop, :boolean
     add_column :patients, :use_billing_address, :boolean
   end
@@ -10,7 +9,6 @@ class AddFieldsToPatientsForPraxistarImport < ActiveRecord::Migration
   def self.down
     remove_column :patients, :created_at
     remove_column :patients, :updated_at
-    remove_column :patients, :remarks
     remove_column :patients, :dunning_stop
     remove_column :patients, :use_billing_address
   end
--- db/migrate/036_add_deceased_remarks_to_patient.rb
+++ db/migrate/036_add_deceased_remarks_to_patient.rb
@@ -1,11 +1,9 @@
 class AddDeceasedRemarksToPatient < ActiveRecord::Migration
   def self.up
     add_column :patients, :deceased, :boolean
-    add_column :patients, :remarks, :string
   end
 
   def self.down
     remove_column :patients, :deceased
-    remove_column :patients, :remarks
   end
 end
