adca2f1a907b47672fb741bdc04fcddfd82d9f7b
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-02-06 14:58:09 +0100

    error_ids field is string, not int

--- db/migrate/066_add_default_for_error_ids_in_imexport.rb
+++ db/migrate/066_add_default_for_error_ids_in_imexport.rb
@@ -1,7 +1,7 @@
 class AddDefaultForErrorIdsInImexport < ActiveRecord::Migration
   def self.up
-    change_column_default :imports, :error_ids, 0
-    change_column_default :exports, :error_ids, 0
+    change_column_default :imports, :error_ids, ""
+    change_column_default :exports, :error_ids, ""
   end
 
   def self.down
