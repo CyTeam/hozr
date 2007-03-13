aa20c9c0e3aad78cbefa3bcb47cddb82c8df4255
simon.huerlimann <simon.huerlimann@zyto-labor.com>
2007-03-13 13:27:36 +0100

    implemented loading indicator for statistic page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
   def get_user
     request.env['REMOTE_USER']
   end
+
+  def loading_indicator_tag(options)
+    image_tag "indicator.gif", :style => "display:none;", :id => loading_indicator_id(options), :alt => "loading indicator", :class => "loading-indicator"
+  end
+
+  def loading_indicator_id(options)
+    if options[:id].nil?
+      "#{options[:scaffold_id]}-#{options[:action]}-loading-indicator"
+    else
+      "#{options[:scaffold_id]}-#{options[:action]}-#{options[:id]}-loading-indicator"
+    end
+  end
 end
