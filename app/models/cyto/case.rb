0224be7792a1b0fa29c0c1648daaa0cab01636c1
Simon Huerlimann <simon.huerlimann@cyt.ch>
2007-05-15 14:50:18 +0200

    make 7 days the default break before leistungsblatt creation

--- app/models/cyto/case.rb
+++ app/models/cyto/case.rb
@@ -173,7 +173,7 @@ class Cyto::Case < ActiveRecord::Base
     save
   end
 
-  def self.praxistar_create_all_leistungsblatt(days_since_print = 4)
+  def self.praxistar_create_all_leistungsblatt(days_since_print = 7)
     export = Praxistar::Exports.new(:started_at => Time.now, :model => self.name)
     
     records = self.find(:all, :conditions => [ "praxistar_leistungsblatt_id IS NULL AND (result_report_printed_at IS NOT NULL AND result_report_printed_at < now() - INTERVAL ? DAY ) AND classification_id IS NOT NULL AND praxistar_eingangsnr > '06/30000'", days_since_print ])
