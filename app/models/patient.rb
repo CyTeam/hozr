2d780607f00fcad96ed9cd771492aac677f9796b
Simon Huerlimann <simon.huerlimann@cyt.ch>
2007-05-16 11:06:55 +0200

    fix date problem with inline edit for patient list

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -29,4 +29,8 @@ class Patient < ActiveRecord::Base
   def birth_date_before_type_cast
     read_attribute(:birth_date).strftime("%d.%m.%Y") unless read_attribute(:birth_date).nil?
   end
+
+  def birth_date
+    read_attribute(:birth_date).strftime("%d.%m.%Y") unless read_attribute(:birth_date).nil?
+  end
 end
