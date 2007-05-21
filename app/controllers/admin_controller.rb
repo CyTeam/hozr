8152d261014dbfa47ab26650947eb39a4db0b514
Simon Huerlimann <simon.huerlimann@logintas.ch>
2007-05-21 15:25:54 +0200

    ajaxify admin actions: import order forms, generate bills, export patients

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,23 +3,20 @@ class AdminController < ApplicationController
   end
 
   def praxistar_create_all_leistungsblatt
-    system "../../script/runner Cyto::Case.praxistar_create_all_leistungsblatt"
+    Cyto::Case.praxistar_create_all_leistungsblatt
     
-    sleep 3
-    render :action => 'praxistar_create_all_leistungsblatt_status'
+    render :partial => 'praxistar_create_all_leistungsblatt_status'
   end
 
   def praxistar_patienten_personalien_export
-    system "../../script/runner Praxistar::PatientenPersonalien.export"
-    
-    sleep 3
-    render :action => 'praxistar_patienten_personalien_export_status'
+    Praxistar::PatientenPersonalien.export
+
+    render :partial => 'praxistar_patienten_personalien_export_status'
   end
 
   def import_order_forms
-    system "../../script/runner Cyto::OrderForm.import_order_forms"
-    
-    sleep 3
-    render :action => 'import_order_forms_status'
+    Cyto::OrderForm.import_order_forms
+
+    render :partial => 'import_order_forms_status'
   end
 end
