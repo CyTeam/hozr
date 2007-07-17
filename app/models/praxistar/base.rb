1574b2b29b45085c7e346bfe00ffcf53da3b6669
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-07-17 08:54:01 +0200

    mssql database is now named zytolabor_$RAILS_ENV, now praxistar_...

--- app/models/praxistar/base.rb
+++ app/models/praxistar/base.rb
@@ -1,7 +1,7 @@
 class Praxistar::Base < ActiveRecord::Base
   require 'yaml'
   
-  praxistar_connection = YAML.load(File.open(File.join(RAILS_ROOT,"config/database.yml"),"r"))["praxistar_"+ ENV['RAILS_ENV']]
+  praxistar_connection = YAML.load(File.open(File.join(RAILS_ROOT,"config/database.yml"),"r"))["zytolabor_"+ ENV['RAILS_ENV']]
   
   establish_connection(praxistar_connection)
 
