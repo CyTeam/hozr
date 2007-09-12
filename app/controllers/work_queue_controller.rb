b2cd7c90893e693730547f4a098332f0d230b7fe
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-09-12 21:52:22 +0200

    add new bills count to admin work_queue

--- app/controllers/work_queue_controller.rb
+++ app/controllers/work_queue_controller.rb
@@ -8,6 +8,7 @@ class WorkQueueController < ApplicationController
     @first_entry_count = Cyto::Case.count(:all, :conditions => 'entry_date IS NULL AND assigned_at IS NOT NULL')
     @print_result_count = Cyto::Case.count(:all, :conditions => '(screened_at IS NOT NULL OR (screened_at IS NULL and needs_p16 = 1 ) ) AND result_report_printed_at IS NULL')
     @bill_export_count = Cyto::Case.count(:all, :conditions => [ "praxistar_leistungsblatt_id IS NULL AND (result_report_printed_at IS NOT NULL AND result_report_printed_at < now() - INTERVAL ? DAY ) AND classification_id IS NOT NULL", 7 ])
+    @bill_print_count = Praxistar::LeistungenBlatt.count_by_sql("SELECT count(*) FROM leistungen_blatt JOIN patienten_personalien ON patient_id = id_patient")
   end
 
   def cyto
