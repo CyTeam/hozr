84ddf5668b6bd92d49c9d0139a75a3945f70377f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-09-12 21:51:09 +0200

    fix count for open first entries

--- app/controllers/work_queue_controller.rb
+++ app/controllers/work_queue_controller.rb
@@ -5,7 +5,7 @@ class WorkQueueController < ApplicationController
 
   def admin
     @assign_count = Cyto::Case.count(:all, :conditions => 'assigned_at IS NULL' )
-    @first_entry_count = Cyto::Case.count(:all, :conditions => 'entry_date IS NULL')
+    @first_entry_count = Cyto::Case.count(:all, :conditions => 'entry_date IS NULL AND assigned_at IS NOT NULL')
     @print_result_count = Cyto::Case.count(:all, :conditions => '(screened_at IS NOT NULL OR (screened_at IS NULL and needs_p16 = 1 ) ) AND result_report_printed_at IS NULL')
     @bill_export_count = Cyto::Case.count(:all, :conditions => [ "praxistar_leistungsblatt_id IS NULL AND (result_report_printed_at IS NOT NULL AND result_report_printed_at < now() - INTERVAL ? DAY ) AND classification_id IS NOT NULL", 7 ])
   end
