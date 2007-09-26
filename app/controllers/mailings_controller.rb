4dd477836b9bfcd27d20c7afaae79846ad971991
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-09-26 12:29:51 +0200

    mailing overview pimping

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -4,8 +4,16 @@ class MailingsController < ApplicationController
     render :action => 'list'
   end
 
-  # Show list of mailings.
+  # Show list of mailings
   def list
-    @mailings = Mailing.find(:all)
+    @mailings = Mailing.find(:all, :order => 'created_at DESC', :limit => 50)
+  end
+
+  # Overview for mailing
+  def overview
+    @mailing = Mailing.find(params[:id])
+    
+    @doctor = @mailing.doctor
+    @cases = @mailing.cases
   end
 end
