ed36f79bc80b5234e09e6d1c258b503ae30caa61
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-10-18 17:01:08 +0200

    add unit test for CaseNr

---
+++ test/unit/cyto/case_nr_test.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Cyto::CaseNrTest < Test::Unit::TestCase
+  fixtures "cyto/cases"
+
+  # Replace this with your real tests.
+  def test_new
+    c = Cyto::CaseNr.new
+    assert_equal c.to_s, '07/00001'
+
+    c = Cyto::CaseNr.new(5)
+    assert_equal c.to_s, '07/00005'
+
+    c = Cyto::CaseNr.new(8, 5)
+    assert_equal c.to_s, '08/00005'
+
+    c = Cyto::CaseNr.new('08/00006')
+    assert_equal c.to_s, '08/00006'
+
+    c = Cyto::CaseNr.new('8/7')
+    assert_equal c.to_s, '08/00007'
+
+    c = Cyto::CaseNr.new(:year => 8, :nr => 8)
+    assert_equal c.to_s, '08/00008'
+  end
+
+  def test_new_without_cases
+    Cyto::Case.destroy_all
+    c = Cyto::CaseNr.new
+    
+    assert_equal "#{Date.today.strftime('%y')}/00001", c.to_s
+  end
+end
