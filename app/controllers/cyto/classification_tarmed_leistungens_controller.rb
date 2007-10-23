1c8ccfbc570f77dc58e8f9f373556eff4c4d6067
Simon Huerlimann <simon.huerlimann@cyt.ch>
2007-10-23 18:05:24 +0200

    some more namespace work in models for rails 1.2

--- app/controllers/cyto/classification_tarmed_leistungens_controller.rb
+++ app/controllers/cyto/classification_tarmed_leistungens_controller.rb
@@ -1,3 +1,5 @@
+include Cyto
+
 class Cyto::ClassificationTarmedLeistungensController < ApplicationController
   def index
     list
@@ -9,7 +11,7 @@ class Cyto::ClassificationTarmedLeistungensController < ApplicationController
          :redirect_to => { :action => :list }
 
   def list
-    @classification_tarmed_leistungen_pages, @classification_tarmed_leistungens = paginate :classification_tarmed_leistungens, :per_page => 10
+    @classification_tarmed_leistungen_pages, @classification_tarmed_leistungens = paginate 'Cyto::ClassificationTarmedLeistungen', :per_page => 10
   end
 
   def show
