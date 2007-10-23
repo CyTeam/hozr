9e86fb4dd78f610f879b843b51da5f4dba257485
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-10-23 15:41:54 +0200

    add name proxy accessor to Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :work_vcard, :class_name => 'Vcard', :foreign_key => :work_vcard_id
   belongs_to :private_vcard, :class_name => 'Vcard', :foreign_key => :private_vcard_id
+
+  # Proxies
+  def name
+    work_vcard.name
+  end
 end
