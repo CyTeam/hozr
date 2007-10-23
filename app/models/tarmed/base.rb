9799ba7941f0f06df3a7055f94cbf72d61ee6942
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-10-23 15:40:20 +0200

    use multidb plugin in Tarmed AR

--- app/models/tarmed/base.rb
+++ app/models/tarmed/base.rb
@@ -1,9 +1,7 @@
 class Tarmed::Base < ActiveRecord::Base
   require 'yaml'
   
-  tarmed_connection = YAML.load(File.open(File.join(RAILS_ROOT,"config/database.yml"),"r"))["tarmed"]
-  
-  establish_connection(tarmed_connection)
+  use_db :prefix => "tarmed_"
 
   def lang
     'D'
