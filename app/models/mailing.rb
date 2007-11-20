e3e40c661c5d4f73f5ec66223c57326e1630880d
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-11-21 00:48:23 +0100

    add self.create(doctor_id, case_ids) to Mailing

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -1,4 +1,13 @@
 class Mailing < ActiveRecord::Base
   belongs_to :doctor
   has_and_belongs_to_many :cases, :class_name => 'Cyto::Case', :order => 'classification_id'
+
+  def self.create(doctor_id, case_ids)
+    mailing = self.new
+    mailing.doctor_id = doctor_id
+    mailing.case_ids = case_ids
+    mailing.save!
+    
+    puts mailing.id
+  end
 end
