d72a469eca2e68667fbb28a96e8fa3f52042dbe1
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-11-29 14:50:21 +0100

    new migration add_printed_at_to_mailings.rb

---
+++ db/migrate/086_add_printed_at_to_mailings.rb
@@ -0,0 +1,9 @@
+class AddPrintedAtToMailings < ActiveRecord::Migration
+  def self.up
+    add_column :mailings, :printed_at, :datetime
+  end
+
+  def self.down
+    remove_column :mailings, :printed_at
+  end
+end
