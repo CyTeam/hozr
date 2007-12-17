34b6114e166ff7c62ab513b0029f9586317111ce
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-12-17 23:10:12 +0100

    add parse_date, date_only_year?, and expand_year to Date

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -105,6 +105,35 @@ module Rubaidh
   end
 end
 
+class Date
+  # Date helpers
+  def self.parse_date(value)
+    if value.is_a?(String)
+      if value.match /.*-.*-.*/
+        return value
+      end
+      day, month, year = value.split('.').map {|s| s.to_i}
+      month ||= Date.today.month
+      year ||= Date.today.year
+      year = expand_year(year, 1900)
+      
+      return sprintf("%4d-%02d-%02d", year, month, day)
+    else
+      return value
+    end
+  end
+  
+  def self.date_only_year?(value)
+    value.is_a?(String) and value.strip.match /^\d{2,4}$/
+  end
+  
+  def self.expand_year(value, base = 1900)
+    year = value.to_i
+    return year < 100 ? year + base : year
+  end
+end
+
+
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
