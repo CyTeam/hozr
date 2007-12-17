c3ded6e02c6bdca4f03669dd9920a6a2bc5562cd
Simon Hürlimann <simon.huerlimann@cyt.ch>
2007-12-17 21:03:32 +0100

    add workload to employees; nicer employee show

---
+++ db/migrate/089_add_workload_to_employees.rb
@@ -0,0 +1,9 @@
+class AddWorkloadToEmployees < ActiveRecord::Migration
+  def self.up
+    add_column :employees, :workload, :float
+  end
+
+  def self.down
+    remove_column :employees, :workload
+  end
+end
