0ed03624f67335c7bf68b7d2e7d9d2edba373cae
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-02-18 16:07:45 +0100

    define bill_type for Praxistar::AccountReceivable returning text like '3. Mahnung'

--- app/models/praxistar/account_receivable.rb
+++ app/models/praxistar/account_receivable.rb
@@ -3,4 +3,17 @@ class Praxistar::AccountReceivable < Praxistar::Base
   set_primary_key "id_debitoren"
 
   belongs_to :bill, :foreign_key => 'Rechnung_ID'
+
+  def bill_type
+    if dt_betreibung
+      return "Betreibung"
+    elsif dt_3Mahnung
+      return "3. Mahnung"
+    elsif dt_2Mahnung
+      return "2. Mahnung"
+    elsif dt_1Mahnung
+      return "1. Mahnung"
+    end
+    return "Rechnung"
+  end
 end
