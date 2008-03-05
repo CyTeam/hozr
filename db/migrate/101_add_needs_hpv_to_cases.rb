6a0e7b3e9f4648df6d5a642d41a985ead4da828f
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-03-05 13:53:03 +0100

    new migration 101_add_needs_hpv_to_cases

---
+++ db/migrate/101_add_needs_hpv_to_cases.rb
@@ -0,0 +1,9 @@
+class AddNeedsHpvToCases < ActiveRecord::Migration
+  def self.up
+    add_column :cases, :needs_hpv, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :cases, :needs_hpv
+  end
+end
