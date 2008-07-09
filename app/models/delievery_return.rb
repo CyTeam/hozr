3fa808284eafd840fdc323a3260400eb70f0387e
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-07-09 13:54:13 +0200

    add DelieveryReturn.close_payed as cronjob method

--- app/models/delievery_return.rb
+++ app/models/delievery_return.rb
@@ -5,4 +5,12 @@ class DelieveryReturn < ActiveRecord::Base
   def before_validation_on_create
     self.cyto_case ||= self.bill.cyto_case
   end
+
+  # Cleanup task, called from cronjob
+  def self.close_payed
+    for d in DelieveryReturn.find(:all, :conditions => ['closed_at IS NULL'])
+      d.closed_at = DateTime.now if (d.cyto_case.bill and (d.cyto_case.bill.payment_state == "payed"))
+      d.save
+    end
+  end
 end
