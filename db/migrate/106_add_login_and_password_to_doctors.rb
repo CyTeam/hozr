314ce6d1d041bb8f369a1bf0f2102c6b0af8df27
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-07-14 14:54:50 +0200

    new migration add_login_and_password_to_doctors

---
+++ db/migrate/106_add_login_and_password_to_doctors.rb
@@ -0,0 +1,11 @@
+class AddLoginAndPasswordToDoctors < ActiveRecord::Migration
+  def self.up
+    add_column :doctors, :login, :string
+    add_column :doctors, :password, :string, :null => false
+  end
+
+  def self.down
+    remove_column :doctors, :login
+    remove_column :doctors, :password
+  end
+end
