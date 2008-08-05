ca563dc3ce545d07b5a48fd5b15ad36f704b5a52
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-08-05 13:28:10 +0200

    add CentrifugeTask and HpvTask as submodels

---
+++ app/models/centrifuge_task.rb
@@ -0,0 +1,9 @@
+class CentrifugeTask < Task
+  def title
+    "Chübele"
+  end
+  
+  def factor
+    return 20.0/48.0
+  end
+end
---
+++ app/models/hpv_task.rb
@@ -0,0 +1,9 @@
+class HpvTask < Task
+  def title
+    "HPV"
+  end
+  
+  def factor
+    return 50
+  end
+end
