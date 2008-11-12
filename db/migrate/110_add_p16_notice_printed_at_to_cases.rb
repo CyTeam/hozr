654da65b61fa7df499381c3756af84380b684a70
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-11-12 15:30:29 +0100

    add migration add_p16_notice_printed_at_to_cases

---
+++ db/migrate/110_add_p16_notice_printed_at_to_cases.rb
@@ -0,0 +1,9 @@
+class AddP16NoticePrintedAtToCases < ActiveRecord::Migration
+  def self.up
+    add_column :cases, :p16_notice_printed_at, :datetime
+  end
+
+  def self.down
+    remove_column :cases, :p16_notice_printed_at
+  end
+end
