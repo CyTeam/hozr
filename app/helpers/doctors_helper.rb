8825f486ce577b6aeecddd77340378ea11b67b72
Simon Hürlimann <simon.huerlimann@cyt.ch>
2008-12-11 15:38:24 +0100

    don't show inactive doctors in doctors select

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,7 +1,7 @@
 module DoctorsHelper
   def select_doctors(params = {})
     method = params[:method] ? params[:method] : :doctor_id
-    collection = Doctor.find(:all, :include => :praxis, :order => 'family_name, given_name').collect { |m| [ [ m.family_name, m.given_name ].join(", ") + " (#{m.praxis.locality})", m.id ] }
+    collection = Doctor.find(:all, :include => :praxis, :order => 'family_name, given_name', :conditions => {:active => true}).collect { |m| [ [ m.family_name, m.given_name ].join(", ") + " (#{m.praxis.locality})", m.id ] }
     
     if params[:form]
       params[:form].select method, collection, :label => 'Arzt', :include_blank => true
