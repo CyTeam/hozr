ea0f2da3812328b347553e6f9050e2e6950266a9
Simon Huerlimann <sh@zyt-deb01-vm.intern.zyto-labor.com>
2010-04-14 11:29:32 +0200

    Use monkey patching to add vcard autocompletion.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,39 +43,33 @@ class ActiveRecord::ConnectionAdapters::MysqlAdapter
 end
 
 
-module ActionController
-  module Macros
-    module AutoComplete
-      module ClassMethods
-        def auto_complete_for_vcard_field(object, method, options = {})
-          case method.to_s
-          when 'street_address', 'locality'
-            model = Address
-          else
-            model = Vcard
-          end
-          
-          define_method("auto_complete_for_#{object}_#{method}") do
-            find_options = {
-              :select => "DISTINCT #{method}",
-              :conditions => [ "LOWER(#{method}) LIKE ?", params[object][method].downcase + '%' ],
-              :order => "#{method} ASC",
-              :limit => 10 }.merge!(options)
-
-            @items = model.find(:all, find_options)
-
-            render :inline => "<%= auto_complete_result @items, '#{method}' %>"
-          end
-        end
-        
-        def auto_complete_for_vcard(object)
-            auto_complete_for_vcard_field object, :family_name, :limit => 12
-            auto_complete_for_vcard_field object, :given_name, :limit => 12
-            auto_complete_for_vcard_field object, :street_address, :limit => 12
-            auto_complete_for_vcard_field object, :locality, :limit => 12
-        end
-      end
+class ActionController::Base
+  def self.auto_complete_for_vcard_field(object, method, options = {})
+    case method.to_s
+    when 'street_address', 'locality'
+      model = Address
+    else
+      model = Vcard
     end
+    
+    define_method("auto_complete_for_#{object}_#{method}") do
+      find_options = {
+        :select => "DISTINCT #{method}",
+        :conditions => [ "LOWER(#{method}) LIKE ?", params[object][method].downcase + '%' ],
+        :order => "#{method} ASC",
+        :limit => 10 }.merge!(options)
+
+      @items = model.find(:all, find_options)
+
+      render :inline => "<%= auto_complete_result @items, '#{method}' %>"
+    end
+  end
+  
+  def self.auto_complete_for_vcard(object)
+      auto_complete_for_vcard_field object, :family_name, :limit => 12
+      auto_complete_for_vcard_field object, :given_name, :limit => 12
+      auto_complete_for_vcard_field object, :street_address, :limit => 12
+      auto_complete_for_vcard_field object, :locality, :limit => 12
   end
 end
 
