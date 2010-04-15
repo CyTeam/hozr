aa91d35aeb68f4ab09d255d7b4798a986747fd8d
Simon Huerlimann <sh@zyt-deb01-vm.intern.zyto-labor.com>
2010-04-15 10:31:59 +0200

    Use Case.paginate in search controller as will_paginate in view requires it.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -245,7 +245,7 @@ class SearchController < ApplicationController
       @cases = []
     else
       case_conditions = [ case_keys.compact.join(" AND "), *case_values ]
-      @cases = Cyto::Case.find :all, :select => 'DISTINCT cases.*', :joins => "LEFT JOIN patients ON patient_id = patients.id LEFT JOIN vcards ON (patients.vcard_id = vcards.id OR patients.billing_vcard_id = vcards.id) LEFT JOIN addresses ON vcards.id = addresses.vcard_id", :conditions => case_conditions, :limit => 100
+      @cases = Cyto::Case.paginate(:page => params['page'], :select => 'DISTINCT cases.*', :joins => "LEFT JOIN patients ON patient_id = patients.id LEFT JOIN vcards ON (patients.vcard_id = vcards.id OR patients.billing_vcard_id = vcards.id) LEFT JOIN addresses ON vcards.id = addresses.vcard_id", :conditions => case_conditions, :per_page => 100)
     end
     
     @include_bill = true
