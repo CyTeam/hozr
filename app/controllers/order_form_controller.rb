358b4f03ec0e262137c5a2ed6e606bdbfb1e8407
Simon Huerlimann <sh@zyt-deb01-vm.intern.zyto-labor.com>
2010-05-11 10:25:46 +0200

    Implement count for order form printing.

--- app/controllers/order_form_controller.rb
+++ app/controllers/order_form_controller.rb
@@ -4,7 +4,7 @@ class OrderFormController < ApplicationController
   
   def print
     @doctor = Doctor.find(params[:order_form][:doctor_id])
-    @count = params[:order_form][:count]
+    @count = params[:order_form][:count].to_i
     
     respond_to do |format|
       format.html {}
@@ -13,9 +13,12 @@ class OrderFormController < ApplicationController
         prawnto :prawn => { :page_size => 'A5', :top_margin => 35, :left_margin => 12, :right_margin => 12, :bottom_margin => 23 }
         page = render_to_string(:layout => false)
         paper_copy = Cups::PrintJob::Transient.new(page, 'hp')
-        paper_copy.print
+        for i in 1..@count
+          paper_copy.print
+        end
         render :update do |page|
-          page.replace_html 'print_flash', "Gedruckt."
+          page.replace_html 'print_flash', "#{@count} Formulare für #{@doctor.name} gedruckt."
+          page.show 'print_flash'
         end
       }
     end
