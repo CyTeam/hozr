7782dbdef8a73bf6802336dfccbf789473c8da27
root <root@new-runner.zyto-labor.com>
2011-02-03 16:13:23 +0100

    Some User model validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
-  # CyDoc
   belongs_to :object, :polymorphic => true
+
+  validates_uniqueness_of   :login
+  
+  # Helpers
+  def to_s
+    object.name
+  end
 end
