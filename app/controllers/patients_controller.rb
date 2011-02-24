8aa8c19860701e1034ddf2e7934634074ad79677
Simon Huerlimann <sh@new-runner.zyto-labor.com>
2011-02-24 15:23:12 +0100

    Port zip/locality extraction from search controller to patients controller.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -63,7 +63,12 @@ class PatientsController < ApplicationController
     keys = []
     values = []
     
-    fields = vcard_params.reject { |key, value| value.nil? or value.empty? }
+    fields = vcard_params.reject { |key, value| value.nil? or value.empty? or key == 'zip_locality'}
+    if (zip_locality = vcard_params[:zip_locality]).present?
+      postal_code, locality = Vcard.parse_zip_locality(zip_locality)
+      fields[:postal_code] = postal_code if postal_code.present?
+      fields[:locality]    = locality if locality.present?
+    end
     fields.each { |key, value|
       keys.push "#{vcard_name}.#{key} LIKE ?"
       values.push '%' + value.downcase.gsub(' ', '%') + '%'
