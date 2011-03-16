04b954b55c4a41f1d09f0560f425d26503cfa083
root <root@new-runner.zyto-labor.com>
2011-03-16 14:31:22 +0100

    Add .to_s to SendQueue.

--- app/models/send_queue.rb
+++ app/models/send_queue.rb
@@ -1,3 +1,9 @@
 class SendQueue < ActiveRecord::Base
   belongs_to :mailing
+
+  # Helpers
+  def to_s
+    # 13.2.2011: 5 Resultate für Muster per e-mail
+    "%s: %i Resultate für %s per %s" % [created_at, mailing.cases.count, channel.to_s, mailing.doctor.to_s]
+  end
 end
