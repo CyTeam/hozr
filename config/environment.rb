bb0edf76e24627703505823c1d9069f10fb211ca
Marc Peter <marc.peter@zyto-labor.com>
2011-03-24 07:10:17 -0700

    Rails update to 2.3.11

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '~>2.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '~>2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -37,10 +37,10 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_hozr_session',
+    :key => '_hozr_session',
     :secret      => 'Use the database for sessions instead of the cookie-based default'
   }
-
+  
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
