ac541894a49db2562992e339b2295f8c2193fc8d
root <root@new-runner.zyto-labor.com>
2011-04-09 11:13:59 +0200

    Label print redesign

--- app/helpers/label_print_helper.rb
+++ app/helpers/label_print_helper.rb
@@ -1,2 +1,5 @@
 module LabelPrintHelper
+def label_select
+	Case.find(:all, :conditions => "entry_date IS NOT NULL AND screened_at IS NULL AND (needs_p16 = '#{Case.connection.false}' AND needs_hpv = '#{Case.connection.false}') AND praxistar_eingangsnr AND praxistar_eingangsnr > '07' AND praxistar_eingangsnr < '90' AND NOT praxistar_eingangsnr LIKE '%-%'").collect { |m| [ m.praxistar_eingangsnr ] }
+  end
 end
