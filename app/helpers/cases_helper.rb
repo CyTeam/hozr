5f15f9afe24dbb0567c2e574f056de84b00084fa
root <root@new-runner.zyto-labor.com>
2011-04-19 15:56:00 +0200

    Port some helper methods from CyLab used for prawn results.

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -21,4 +21,25 @@ module CasesHelper
     css_class = "finding_class_#{finding.code} "
     css_class += finding.finding_groups.collect { |group| "finding_group_#{group.name}" }.join(' ')
   end
+
+  # From CyLab
+  def patient_nr
+    return nil if @case.patient.doctor_patient_nr.blank?
+
+    return "##{@case.patient.doctor_patient_nr.strip}"
+  end
+
+  def patient_birth_date
+    return "" if @case.patient.birth_date.nil?
+
+#    CyLab has no implicit cast:
+#    return @case.patient.birth_date.strftime("%d.%m.%Y")
+    return @case.patient.birth_date
+  end
+
+  def insurance_policy
+    return "" if @case.insurance.nil?
+
+    return @case.insurance.vcard.full_name + (@case.insurance_nr.present? ? " #{@case.insurance_nr.strip}" : "")
+  end
 end
--- app/models/classification_group.rb
+++ app/models/classification_group.rb
@@ -1,3 +1,33 @@
 class ClassificationGroup < ActiveRecord::Base
   has_many :classifications
+
+  def color
+    case title
+    when 'Azellulär', 'PAP I', 'PAP II'
+      'ffffff'
+    when 'PAP II agus/ascus', 'PAP II-III', 'PAP II-III agus/ascus'
+      'ffff00'
+    when 'PAP III'
+      '0066ff'
+    when 'PAP IV', 'PAP V'
+      'ff0000'
+    when 'Mama', 'Sputum', 'HPV', 'Urin', 'Extra-gynäkologisches Material'
+      '00cc00'
+    end
+  end
+
+  def print_color
+    case title
+    when 'Azellulär', 'PAP I', 'PAP II'
+      'ffffff'
+    when 'PAP II agus/ascus', 'PAP II-III', 'PAP II-III agus/ascus'
+      'ffff88'
+    when 'PAP III'
+      'bbaaff'
+    when 'PAP IV', 'PAP V'
+      'ff9191'
+    when 'Mama', 'Sputum', 'HPV', 'Urin', 'Extra-gynäkologisches Material'
+      '74cc74'
+    end
+  end
 end
