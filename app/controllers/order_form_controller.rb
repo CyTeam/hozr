2af2513c3c13a8561d3e8d360883e5c007b1d5a6
root <root@new-runner.zyto-labor.com>
2011-04-28 15:27:26 +0200

    Use CUPS options for copies in order form printing.

--- app/controllers/order_form_controller.rb
+++ app/controllers/order_form_controller.rb
@@ -4,7 +4,7 @@ class OrderFormController < ApplicationController
   
   def print
     @doctor = Doctor.find(params[:order_form][:doctor_id])
-    @count = params[:order_form][:count].to_i
+    @count = params[:order_form][:count]
     
     respond_to do |format|
       format.html {}
@@ -12,10 +12,16 @@ class OrderFormController < ApplicationController
         # Use A5
         prawnto :prawn => { :page_size => 'A5', :top_margin => 35, :left_margin => 12, :right_margin => 12, :bottom_margin => 23 }
         page = render_to_string(:layout => false)
-        paper_copy = Cups::PrintJob::Transient.new(page, 'hp')
-        for i in 1..@count
-          paper_copy.print
-        end
+        options = {'copies' => @count}
+
+        # Workaround TransientJob not yet accepting options
+        file = Tempfile.new('')
+        file.puts(page)
+        file.close
+
+        paper_copy = Cups::PrintJob.new(file.path, 'hp', options)
+        paper_copy.print
+
         render :update do |page|
           page.replace_html 'print_flash', "#{@count} Formulare für #{@doctor.name} gedruckt."
           page.show 'print_flash'
