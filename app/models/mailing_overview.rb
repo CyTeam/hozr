e9da7a3ab2199ccccf10db2cd83ddcfcdbb17dfd
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-06-23 14:50:01 +0200

    Copy/paste to get P16 notice reports included again.

--- app/models/mailing_overview.rb
+++ app/models/mailing_overview.rb
@@ -127,6 +127,79 @@ class MailingOverview < Prawn::Document
       move_down 10
     end
     
+    group_cases = @mailing.doctor.cases.unfinished_p16.find(:all, :order => 'praxistar_eingangsnr')
+
+    unless group_cases.empty?
+      move_down 10
+      
+      # TODO: massiv copy/paste!
+      # Table content creation.
+      items = group_cases.map do |item|
+        row = [
+          make_cell(:content => item.patient.name),
+          make_cell(:content => item.patient.birth_date),
+          make_cell(:content => 'Resultat folgt innert Tagen'),
+          make_cell(:content => item.examination_date.strftime('%d.%m.%Y')),
+          make_cell(:content => item.praxistar_eingangsnr)
+        ]
+      end
+
+      # Table header creation.
+      headers = [[
+        make_cell(:content => "P16 <i>(#{items.count})</i>", :inline_format => true),
+        "Geb.",
+         "",
+         "Entn.",
+         "Eing.Nr."
+      ]]
+
+      # Table creation.
+      table headers + items, :header => true,
+                                 :width => margin_box.width,
+                                 :cell_style => { :overflow => :shrink_to_fit, :min_font_size => 8 } do
+
+        # General cell styling
+        cells.align  = :left
+        cells.valign = :top
+        cells.border_width = 0
+        cells.size = 10
+        cells.padding = [0, 5, 0, 5]
+        column(0).padding_left = 0
+        column(-1).padding_right = 0
+
+        # Headings styling
+        row(0).font_style = :italic
+        row(0).column(0).font_style = :bold
+        row(0).padding_top = 3
+        row(0).padding_bottom = 3
+        row(0).background_color = group.print_color
+
+        # Footer
+        row(-1).borders = [:bottom]
+        row(-1).border_width = 1
+        row(-1).border_color = 'CCCCCC'
+        row(-1).padding_bottom = 5
+
+        # Columns width
+        column(1).width = 1.8.cm
+        column(2).width = 6.cm
+        column(3).width = 1.8.cm
+        column(4).width = 1.8.cm
+
+        # Columns align
+        column(1).align = :right
+        column(3).align = :right
+        column(4).align = :right
+
+        # Columns colors
+        column(0).row(1..-1).text_color = '660000'
+        column(4).row(1..-1).text_color = '660000'
+
+        # Columns style
+        column(0).font_style = :bold
+        column(2).font_style = :bold
+      end
+    end
     render
   end
 end
