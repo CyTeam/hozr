64393f3c1ba775466223d11f1931f9b9d1c53b10
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-07-22 13:14:41 +0200

    Drop unused tests.

--- test/unit/helpers/post_etiketten_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PostEtikettenHelperTest < ActionView::TestCase
-end
--- test/unit/tarmed/tarmed_test_helper.rb
+++
@@ -1,4 +0,0 @@
-unless defined?(MIGRATED_SEC_DB_FOR_TEST)
-  UseDbTest.prepare_test_db(:prefix => "tarmed_")
-  MIGRATED_SEC_DB_FOR_TEST = true
-end
