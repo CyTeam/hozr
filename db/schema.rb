b659c42816bc437ac146c3dba2952afa1e5e11b0
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-07-22 14:50:03 +0200

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110722111049) do
+ActiveRecord::Schema.define(:version => 20110722124810) do
 
   create_table "account_receivables", :force => true do |t|
   end
@@ -380,7 +381,7 @@ ActiveRecord::Schema.define(:version => 20110722111049) do
     t.text     "type"
   end
 
-  create_table "top_finding_classes", :force => true do |t|
+  create_table "top_finding_classes", :id => false, :force => true do |t|
     t.integer "classification_id"
     t.integer "finding_class_id"
   end
