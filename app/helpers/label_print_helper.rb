fd6db46a0d0364aca5f52c31c3730d4a64ff5ad1
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 11:47:37 +0200

    Drop duplicate doctors_collection from label_print helper.

--- app/helpers/label_print_helper.rb
+++ app/helpers/label_print_helper.rb
@@ -3,11 +3,7 @@ module LabelPrintHelper
     Case.for_second_entry.collect { |m| [ m.praxistar_eingangsnr ] }
   end
 
-  def doctors_collection
-    Doctor.find(:all, :include => {:praxis => :address}, :order => 'vcards.family_name, vcards.given_name', :conditions => {:active => true}).collect { |m| [ [ m.family_name, m.given_name ].join(", ") + " (#{m.praxis.locality})", m.id ] }
-  end
-
   def label
-   Vcard.find(Doctor.find(params[:order_form][:doctor_id]).praxis_vcard)
+    Vcard.find(Doctor.find(params[:order_form][:doctor_id]).praxis_vcard)
   end
 end
