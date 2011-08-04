7e51cc245ef94633934f7ba821f6509d4c932dc2
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 13:07:51 +0200

    Use for_second_entry.count in work_queue.

--- app/controllers/work_queue_controller.rb
+++ app/controllers/work_queue_controller.rb
@@ -6,7 +6,7 @@ class WorkQueueController < ApplicationController
   def admin
     @assign_count = Case.count(:all, :conditions => 'assigned_at IS NULL' )
     @first_entry_count = Case.count(:all, :conditions => 'entry_date IS NULL AND assigned_at IS NOT NULL')
-    @second_entry_count = Case.count(:all, :conditions => "entry_date IS NOT NULL AND screened_at IS NULL AND (needs_p16 = '#{Case.connection.false}' AND needs_hpv = '#{Case.connection.false}') AND praxistar_eingangsnr > '07' AND praxistar_eingangsnr < '90' AND NOT praxistar_eingangsnr LIKE '%-%'")
+    @second_entry_count = Case.for_second_entry.count
     @p16_count = Case.count(:all, :conditions => ["needs_p16 = ? AND screened_at IS NULL", true])
     @hpv_count = Case.count(:all, :conditions => ["needs_hpv = ? AND screened_at IS NULL", true])
     @p16_prepared_count = Case.count(:all, :conditions => ["needs_p16 = ? AND screened_at IS NULL AND hpv_p16_prepared_at is not NULL", true])
