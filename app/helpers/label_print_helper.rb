a4d543a8ed69346c1b6698c22c2785a0aa2ca5c7
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 11:53:50 +0200

    Add intraday id to case select in label_print.

--- app/helpers/label_print_helper.rb
+++ app/helpers/label_print_helper.rb
@@ -1,5 +1,5 @@
 module LabelPrintHelper
   def label_select
-    Case.for_second_entry.collect { |m| [ m.praxistar_eingangsnr ] }
+    Case.for_second_entry.collect { |m| [ "#{m.praxistar_eingangsnr} (#{m.intra_day_id})", m.praxistar_eingangsnr ] }
   end
 end
