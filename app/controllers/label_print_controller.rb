68931796ac7b78f586590559f47c5bfb260f347b
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 10:53:00 +0200

    Whitespace fixes in label_print_controller.

--- app/controllers/label_print_controller.rb
+++ app/controllers/label_print_controller.rb
@@ -11,12 +11,12 @@ class LabelPrintController < ApplicationController
     
     # Create new record
     po_label = Aetiketten.new
-    po_label.hon_suffix=label.honorific_prefix
-    po_label.fam_name=label.family_name
-    po_label.giv_name=label.given_name
-    po_label.ext_address=label.address.street_address
-    po_label.loc=label.locality
-    po_label.postc=label.postal_code
+    po_label.hon_suffix = label.honorific_prefix
+    po_label.fam_name = label.family_name
+    po_label.giv_name = label.given_name
+    po_label.ext_address = label.address.street_address
+    po_label.loc = label.locality
+    po_label.postc = label.postal_code
     po_label.save
 
     # Trigger printing
@@ -39,13 +39,13 @@ class LabelPrintController < ApplicationController
     label_delete
     for label in @cases
       ot_label = OtLabel.new
-      ot_label.sys_id=label.id
-      ot_label.prax_nr=label.praxistar_eingangsnr
-      ot_label.doc_fname=label.doctor.family_name
-      ot_label.doc_gname=label.doctor.given_name
-      ot_label.pat_fname=label.patient.vcard.family_name
-      ot_label.pat_gname=label.patient.vcard.given_name
-      ot_label.pat_bday=label.patient.birth_date
+      ot_label.sys_id = label.id
+      ot_label.prax_nr = label.praxistar_eingangsnr
+      ot_label.doc_fname = label.doctor.family_name
+      ot_label.doc_gname = label.doctor.given_name
+      ot_label.pat_fname = label.patient.vcard.family_name
+      ot_label.pat_gname = label.patient.vcard.given_name
+      ot_label.pat_bday = label.patient.birth_date
       ot_label.save
     end
   end
