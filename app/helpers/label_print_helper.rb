a50440be8949ae70d10dc8f0bb687ac220842052
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 11:48:30 +0200

    Drop unused label helper in label_print helper.

--- app/helpers/label_print_helper.rb
+++ app/helpers/label_print_helper.rb
@@ -2,8 +2,4 @@ module LabelPrintHelper
   def label_select
     Case.for_second_entry.collect { |m| [ m.praxistar_eingangsnr ] }
   end
-
-  def label
-    Vcard.find(Doctor.find(params[:order_form][:doctor_id]).praxis_vcard)
-  end
 end
