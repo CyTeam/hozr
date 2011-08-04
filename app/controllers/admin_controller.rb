174184b1e0048180b2b985fd2cd1692aeb0fb67f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 13:38:43 +0200

    Drop unused import_order_forms action.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -22,12 +22,6 @@ class AdminController < ApplicationController
     render :partial => 'praxistar_patienten_personalien_export_status'
   end
 
-  def import_order_forms
-    OrderForm.import_order_forms
-
-    render :partial => 'import_order_forms_status'
-  end
-
   def batch_reactivate
     bill_params = params[:bill]
     Praxistar::Bill.batch_reactivate(bill_params[:ids], bill_params[:reason])
