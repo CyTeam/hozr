4926d8be5c545900b84c6ba34a54092e5d8e3901
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-04 10:31:39 +0200

    Whitespace fixes in label_print_controller.

--- app/controllers/label_print_controller.rb
+++ app/controllers/label_print_controller.rb
@@ -2,31 +2,31 @@ class LabelPrintController < ApplicationController
   helper :label_print
   
   def label
-      @start_praxnr = params[:start_praxnr]
-      @end_praxnr = params[:end_praxnr]
-      @cases = Case.find(:all, :conditions => ["entry_date IS NOT NULL AND screened_at IS NULL AND (needs_p16 = '#{Case.connection.false}' AND needs_hpv = '#{Case.connection.false}') AND praxistar_eingangsnr BETWEEN ? AND ? AND praxistar_eingangsnr > '01' AND praxistar_eingangsnr < '90' AND NOT praxistar_eingangsnr LIKE '%-%'",@start_praxnr,@end_praxnr ])
-      label_save
-      set_triger
+    @start_praxnr = params[:start_praxnr]
+    @end_praxnr = params[:end_praxnr]
+    @cases = Case.find(:all, :conditions => ["entry_date IS NOT NULL AND screened_at IS NULL AND (needs_p16 = '#{Case.connection.false}' AND needs_hpv = '#{Case.connection.false}') AND praxistar_eingangsnr BETWEEN ? AND ? AND praxistar_eingangsnr > '01' AND praxistar_eingangsnr < '90' AND NOT praxistar_eingangsnr LIKE '%-%'",@start_praxnr,@end_praxnr ])
+    label_save
+    set_triger
   end
 
   def post_label
-	post_label_print
+    post_label_print
   end
    
   def post_label_print
-	label = Vcard.find(Doctor.find(params[:order_form][:doctor_id]).praxis_vcard)
-	po_label = Aetiketten.new
-	po_label.hon_suffix=label.honorific_prefix
-	po_label.fam_name=label.family_name
-	po_label.giv_name=label.given_name
-	po_label.ext_address=label.praxis.address.street_address
-	po_label.loc=label.praxis.locality
-	po_label.postc=label.praxis.postal_code
-	po_label.save
+    label = Vcard.find(Doctor.find(params[:order_form][:doctor_id]).praxis_vcard)
+    po_label = Aetiketten.new
+    po_label.hon_suffix=label.honorific_prefix
+    po_label.fam_name=label.family_name
+    po_label.giv_name=label.given_name
+    po_label.ext_address=label.praxis.address.street_address
+    po_label.loc=label.praxis.locality
+    po_label.postc=label.praxis.postal_code
+    po_label.save
   end 
 
   def post_label_delete
-	Aetiketten.delete_all
+    Aetiketten.delete_all
   end
 
   def label_save
