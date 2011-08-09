e52a528fc6d2aee3b3c11e0ebe318b27e458bc72
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-09 12:44:11 +0200

    Use email for User.to_s if no object associated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :trackable, :validatable, :timeoutable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
   belongs_to :object, :polymorphic => true
 
   validates_uniqueness_of   :login
   
   # Helpers
   def to_s
-    object.name
+    if object
+      return object.name
+    else
+      return email
+    end
   end
 end
