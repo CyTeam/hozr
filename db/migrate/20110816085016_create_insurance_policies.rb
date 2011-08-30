7f3b551e1ee03eb456503e4edc441caff8060952
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-30 13:56:35 +0200

    Use INSERT INTO ... SELECT to speed insurance_policy migration from hours to seconds.

--- db/migrate/20110816085016_create_insurance_policies.rb
+++ db/migrate/20110816085016_create_insurance_policies.rb
@@ -1,16 +1,8 @@
 class CreateInsurancePolicies < ActiveRecord::Migration
   def self.up
-    for patient in Patient.all
-      puts patient
-
-      policy = patient.insurance_policies.build
-
-      policy.insurance_id = patient.insurance_id
-      policy.number       = patient.insurance_nr
-      policy.policy_type  = "KVG"
-      
-      patient.save(:validate => false)
-    end
+    Patient.connection.execute(
+      "INSERT INTO insurance_policies (patient_id, insurance_id, number, policy_type) SELECT id, insurance_id, insurance_nr, 'KVG' FROM patients"
+    )
   end
 
   def self.down
