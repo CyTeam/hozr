1c7ed1f9eabfe224448fb23423b42183d9547dd6
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-10-26 15:23:39 +0200

    Drop bills controller/views.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,25 +3,6 @@ class AdminController < ApplicationController
 
   def index
   end
-
-  def praxistar_create_all_leistungsblatt
-    Case.praxistar_create_all_leistungsblatt
-    
-    @exports = Praxistar::Exports.where(:model => 'Case').order('finished_at DESC').limit(10).all
-
-    render :partial => 'praxistar_create_all_leistungsblatt_status'
-  end
-
-  def praxistar_create_leistungsblatt
-    Case.find(params[:id]).praxistar_create_leistungsblatt
-    
-    redirect_to :controller => 'search', :action => ''
-  end
-
-  def batch_reactivate
-    bill_params = params[:bill]
-    Praxistar::Bill.batch_reactivate(bill_params[:ids], bill_params[:reason])
-  end
   
   def unsign_case
     a_case = Case.find(params[:id])
