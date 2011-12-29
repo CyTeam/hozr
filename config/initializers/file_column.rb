3917d3cbeb3d297d2d4e6a70737732d805228a9f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-29 14:04:11 +0100

    Use public/system for file_column storage.

---
+++ config/initializers/file_column.rb
@@ -0,0 +1,2 @@
+FileColumn::ClassMethods::DEFAULT_OPTIONS[:root_path] = Rails.root.join("public", "system")
+FileColumn::ClassMethods::DEFAULT_OPTIONS[:web_root] = "system/"                           
