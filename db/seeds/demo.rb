8a67a856a059a9ec0585081466146354562ae816
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-30 11:27:15 +0100

    Fix admin user seed to assign employee, not doctor; new seed user zyto.

--- db/seeds/demo.rb
+++ db/seeds/demo.rb
@@ -5,15 +5,31 @@ tenant = Doctor.create!(
   :praxis => Vcard.new(:family_name => "Labori", :given_name => "Andre", :street_address => "Hauptstr. 99", :postal_code => "6300", :locality => "Zug", :vcard_type => "praxis")
 )
 
-# User
+# Employees
+zyto = Employee.create!(
+  :work_vcard => Vcard.new(:family_name => "Zyti", :given_name => "Christa", :street_address => "Arbeitsweg 8", :postal_code => "6300", :locality => "Zug", :vcard_type => "work")
+)
+user = User.create!(
+  :login => 'zyto',
+  :email => 'zyto@example.com',
+  :password => 'zyto1234',
+  :password_confirmation => 'zyto1234',
+  :object => zyto,
+  :role_texts => ['zyto']
+)
+
+sysadmin = Employee.create!(
+  :work_vcard => Vcard.new(:family_name => "Alleskönner", :given_name => "Petra", :street_address => "Tankstellenallee 55", :postal_code => "6300", :locality => "Zug", :vcard_type => "work")
+)
 user = User.create!(
   :login => 'admin',
   :email => 'admin@example.com',
   :password => 'admin1234',
   :password_confirmation => 'admin1234',
-  :object => tenant,
+  :object => sysadmin,
   :role_texts => ['sysadmin', 'zyto']
 )
+
 
 # Doctors
 Doctor.create!(
