179b5d486222d6d850f611da90ba5f6c4aa4f13c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-03 13:59:58 +0100

    Add classifications to demo seed.

--- db/seeds/demo.rb
+++ db/seeds/demo.rb
@@ -39,3 +39,30 @@ Doctor.create!(
 doctor = Doctor.create!(
   :praxis => Vcard.new(:family_name => "Klientes", :given_name => "Melanie", :street_address => "Zentralgasse 99", :postal_code => "6300", :locality => "Zug", :vcard_type => "praxis")
 )
+
+# Classifications
+pap_group = ClassificationGroup.create!(
+  :title => "PAP",
+  :position => 1,
+  :color => "white",
+  :print_color => "none"
+)
+
+eg_group = ClassificationGroup.create!(
+  :title => "Extra Gyn",
+  :position => 2,
+  :color => "green",
+  :print_color => "green"
+)
+
+for examination_method in ExaminationMethod.all
+  Classification.create!([
+    {:name => "PAP I", :code => "PAP I", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP II", :code => "PAP II", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP III", :code => "PAP III", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP IIID", :code => "PAP IIID", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP IIIG", :code => "PAP IIIG", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP IV", :code => "PAP IV", :classification_group => pap_group, :examination_method => examination_method},
+    {:name => "PAP V", :code => "PAP V", :classification_group => pap_group, :examination_method => examination_method},
+  ])
+end
