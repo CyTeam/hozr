a1be2a458020a0da1fef9b0d97a2932620bf2f6f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-09 15:31:03 +0100

    Add Classification <-> ExaminationMethod association.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 class Classification < ActiveRecord::Base
-  belongs_to :examination_method
   belongs_to :classification_group
   has_and_belongs_to_many :top_finding_classes, :join_table => 'top_finding_classes', :class_name => 'FindingClass', :foreign_key => 'classification_id'
+
+  # Examination method
+  belongs_to :examination_method
 end
--- app/models/examination_method.rb
+++ app/models/examination_method.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
 
 class ExaminationMethod < ActiveRecord::Base
+  # Classifications
+  has_many :classifications
 end
