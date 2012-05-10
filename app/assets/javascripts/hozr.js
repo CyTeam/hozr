e2bdcaf5b157e8a7a4a8848bef3eb469d2482cef
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-05-10 14:33:15 +0200

    Port slidepath links to UJS.

---
+++ app/assets/javascripts/hozr.js
@@ -0,0 +1,16 @@
+function setupSlidepathLinks() {
+  $('.slidepath_links').each(function() {
+    $.ajax({
+        type: 'GET',
+        url: '/slidepath/links',
+        data: 'case_id=' + $(this).data('case-id'),
+        success: function(data){eval(data);}
+    });
+  });
+};
+
+function initializeBehaviours() {
+  setupSlidepathLinks();
+}
+
+$(document).ready(initializeBehaviours);
