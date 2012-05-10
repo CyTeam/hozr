dea1f44ab8ec7f3aab3fda4acec87d41ae94861f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-05-10 13:52:37 +0200

    Get rid of insurance action hiding.

--- app/assets/javascripts/hozr.js
+++
@@ -1,7 +0,0 @@
-function showActions(id) {
-  document.getElementById('actions_' + id).style.display = 'inline'
-}
-
-function hideActions(id) {
-  document.getElementById('actions_' + id).style.display = 'none'
-}
