64de6b7a798c54d3901d0145c001ff2ef3d7913f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-05-15 11:26:49 +0200

    Port case assignment JS to UJS.

--- app/assets/javascripts/hozr.js
+++ app/assets/javascripts/hozr.js
@@ -1,3 +1,33 @@
+// Case assignment
+function getValueFromPreviousCase(field, input) {
+  var case_id = $(input).parents('.case').data('case-id');
+  return $('#a_case_' + (case_id - 1) + '_' + field).val();
+}
+
+function guessIntraDayIdFromPreviousCase() {
+  var value = getValueFromPreviousCase('intra_day_id', this);
+  if (value === undefined) {
+    return
+  } else {
+    $(this).val(parseInt(value) + 1);
+  }
+}
+
+function guessExaminationDateFromPreviousCase() {
+  $(this).val(getValueFromPreviousCase('examination_date', this));
+}
+
+function guessDoctorIdFromPreviousCase() {
+  $(this).val(getValueFromPreviousCase('doctor_id', this));
+}
+
+function setupCaseAssignment() {
+  $("[name$='[intra_day_id]']").live('focus', guessIntraDayIdFromPreviousCase);
+  $("[name$='[examination_date]']").live('focus', guessExaminationDateFromPreviousCase);
+  $("[name$='[doctor_id]']").live('focus', guessDoctorIdFromPreviousCase);
+}
+
+// SlidePath
 function setupSlidepathLinks() {
   $('.slidepath_links').each(function() {
     $.ajax({
@@ -11,6 +41,7 @@ function setupSlidepathLinks() {
 
 function initializeBehaviours() {
   setupSlidepathLinks();
+  setupCaseAssignment();
 }
 
 $(document).ready(initializeBehaviours);
