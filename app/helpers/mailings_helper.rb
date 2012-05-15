90093c600e93a24e2fcdbe58b0ba26d58ff20fc1
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-05-15 13:47:39 +0200

    Drop unused MailingsHelper.

--- app/helpers/mailings_helper.rb
+++
@@ -1,5 +0,0 @@
-module MailingsHelper
-  def link_to_send_by(mailing, channel)
-#    link_to_remote(t(channel, :scope => 'channel'), :update => "mailing_log_#{mailing.id}", :loading => "Element.show('#{loading_indicator_id({:id => "mailing_#{mailing.id}"})}')", :url => {:controller => :mailings, :action => :send_by, :channel => channel, :id => mailing }) if mailing.doctor.wants(channel)
-  end
-end
