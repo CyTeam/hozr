1641be3e06c1fc46564d5368e543d964db8835a9
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-05-31 10:15:53 +0200

    Use POST method for label_print/case_label_p16.

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,7 @@ Hozr::Application.routes.draw do
 
   # Label Printing
   get 'label_print/case_label' => 'label_print#case_label', :as => :case_label_label_print
-  get 'label_print/case_label_p16' => 'label_print#case_label_p16', :as => :case_label_p16_label_print
+  post 'label_print/case_label_p16' => 'label_print#case_label_p16', :as => :case_label_p16_label_print
   get 'label_print/post_label' => 'label_print#post_label', :as => :post_label_label_print
 
   # Search
