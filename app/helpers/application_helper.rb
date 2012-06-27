d7010d3309dc199bc3ddfbb9e2fa4c63c8fa93fa
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-06-27 13:29:21 +0200

    Drop loading_indicator instances.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  # AJAX helpers
-  def loading_indicator_tag(options)
-    image_tag "indicator.gif", :style => "display:none;", :id => loading_indicator_id(options), :alt => "loading indicator", :class => "loading-indicator"
-  end
-
-  def loading_indicator_id(options)
-    if options[:id].nil?
-      "#{options[:scaffold_id]}-#{options[:action]}-loading-indicator"
-    else
-      "#{options[:scaffold_id]}-#{options[:action]}-#{options[:id]}-loading-indicator"
-    end
-  end
-
   # Unescape HTML
   HTML_ESCAPES = [
     ['&uuml;', 'ü'],
