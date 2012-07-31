31cfdb7dd527e633e3f7f5586b7de8aa6243613a
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-07-31 13:27:09 +0200

    Drop guard in order_form_head_image helper.

--- app/helpers/order_forms_helper.rb
+++ app/helpers/order_forms_helper.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8'
 module OrderFormsHelper
   def order_form_head_image(order_form, header_image_type)
-    return unless order_form
-    
     link_to image_tag(inline_order_form_url(order_form, :type => header_image_type )), inline_order_form_url(order_form)
   end
 end
