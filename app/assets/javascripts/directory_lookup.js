d7088a8c0792f8a601cc6e7cb58bd1322189271e
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-09-11 15:12:22 +0200

    Implement JS updating of address fields from directory_lookup.

--- app/assets/javascripts/directory_lookup.js
+++ app/assets/javascripts/directory_lookup.js
@@ -7,13 +7,28 @@ function showDirectoryLookupDialog(selector) {
 }
 
 function updateFromDirectoryLookup(selector, params) {
-  $("input[name^='patient[billing_vcard_attributes][family_name]']").val(v.data('directory-lookup-attributes')['family_name'])
+  ['family_name', 'given_name'].map(function(attribute) {
+    $("input[name^='" + selector + "[" + attribute + "]']").val(params[attribute]);
+  });
+
+  ['street_address', 'locality', 'postal_code'].map(function(attribute) {
+    $("input[name^='" + selector + "[address_attributes][" + attribute + "]']").val(params[attribute]);
+  });
+}
+
+function directoryLookupHandler() {
+  var selector = $(this).data('directory-lookup-selector');
+  var params = $(this).data('directory-lookup-attributes');
+
+  updateFromDirectoryLookup(selector, params);
 }
 
 
 function addDirectoryLookupBehaviour() {
-  $('[data-directory-lookup]').on('click', function(event) {
+  $('body').on('click', '[data-directory-lookup]', function(event) {
     var selector = $(this).data('directory-lookup');
     showDirectoryLookupDialog(selector);
   })
+
+  $('body').on('click', '[data-directory-lookup-attributes]', directoryLookupHandler);
 }
