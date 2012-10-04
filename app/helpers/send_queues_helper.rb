9bbe1ea8db09e14792c03f8225ca6966aff498b9
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-10-04 10:18:54 +0200

    Use labels for send queue channel types.

---
+++ app/helpers/send_queues_helper.rb
@@ -0,0 +1,18 @@
+module SendQueuesHelper
+  def channel_label(channel)
+    type = ''
+    case channel.to_s
+    when 'print'
+    when 'hl7'
+      type = 'info'
+    when 'email', 'overview_email'
+      type = 'warning'
+    end
+
+    boot_label(channel, type)
+  end
+
+  def channel_labels(channels)
+    channels.map{|channel| channel_label(channel)}.join(' ').html_safe
+  end
+end
