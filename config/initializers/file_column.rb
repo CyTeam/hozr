4d5d43150c21c3c2386dedb9bb31e06065f30d21
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-06 16:00:13 +0100

    Drop FileColumn initializer.

--- config/initializers/file_column.rb
+++
@@ -1,2 +0,0 @@
-FileColumn::ClassMethods::DEFAULT_OPTIONS[:root_path] = Rails.root.join("public", "system")
-FileColumn::ClassMethods::DEFAULT_OPTIONS[:web_root] = "system/"                           
