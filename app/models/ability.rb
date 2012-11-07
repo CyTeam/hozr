0889e85423902e272f50b7d2db584a7a2d019ed7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-07 12:42:59 +0100

    Port to properly use lyb_devise_admin.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,16 @@ class Ability
   # Aspects
   include CanCan::Ability
 
+  # Available roles
+  def self.roles
+    Role.pluck(:name)
+  end
+
+  # Prepare roles to show in select inputs etc.
+  def self.roles_for_collection
+    self.roles.map{|role| [I18n.translate(role, :scope => 'cancan.roles'), role]}
+  end
+
   # Main role/ability definitions.
   def initialize(user)
     alias_action :index, :to => :list
