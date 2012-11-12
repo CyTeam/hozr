085dcc2d4747b9e81356c34f4303cd5a83a09b57
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-12 13:05:11 +0100

    Add package dependency detection for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,18 @@ set :use_sudo, false
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :copy_exclude, [".git", "spec"]
+
+# Dependencies
+depend :remote, :gem, 'bundler', '> 0'
+
+# Headers for gem compilation
+depend :remote, :deb, "build-essential", ''
+depend :remote, :deb, "ruby1.9.1-dev", ''
+depend :remote, :deb, "libcups2-dev", ''
+depend :remote, :deb, "libmysqlclient-dev", ''
+depend :remote, :deb, "libsqlite3-dev", ''
+depend :remote, :deb, "libxml2-dev", ''
+depend :remote, :deb, "libxslt1-dev", ''
+depend :remote, :deb, "libmagickcore-dev", ''
+depend :remote, :deb, "libmagickwand-dev", ''
+depend :remote, :deb, "sphinxsearch", ''
