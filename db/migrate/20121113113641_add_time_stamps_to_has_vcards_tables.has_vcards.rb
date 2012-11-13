9fae0ac06e9684db4b1e935035a0734d91fa1dab
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-13 12:58:04 +0100

    Add has_vcards migrations as dummies.

---
+++ db/migrate/20121113113641_add_time_stamps_to_has_vcards_tables.has_vcards.rb
@@ -0,0 +1,10 @@
+# This migration comes from has_vcards (originally 20120813065226)
+
+# Dummy migration
+class AddTimeStampsToHasVcardsTables < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/20121113113642_create_honorific_prefixes_table.has_vcards.rb
@@ -0,0 +1,10 @@
+# This migration comes from has_vcards (originally 20121113120000)
+
+# Dummy migration
+class CreateHonorificPrefixesTable < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
