5fe9679d9c55ddf626500afe770dd089586da0cd
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-12-04 10:30:22 +0100

    Revert "Comment out Slidepath::LocationIndex."
    This reverts commit 92bc3819a5cf27f3ceb109023b29b70cc298e50e.

--- app/models/slidepath/location_index.rb
+++ app/models/slidepath/location_index.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
-#class Slidepath::LocationIndex < ActiveRecord::Base
-#  # Legacy specs
-#  use_db :prefix => 'slidepath_'
-#  self.table_name = 'location_index'
-#  self.primary_key = 'locationId'
-#
-#  # Case
-#  def a_case
-#    Case.find_by_id(fileName.first(6))
-#  end
-#end
+class Slidepath::LocationIndex < ActiveRecord::Base
+  # Legacy specs
+  use_db :prefix => 'slidepath_'
+  self.table_name = 'location_index'
+  self.primary_key = 'locationId'
+
+  # Case
+  def a_case
+    Case.find_by_id(fileName.first(6))
+  end
+end
