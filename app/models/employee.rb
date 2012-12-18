f32746deeede69a77040aa73e3b0a21b95028efb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-12-18 23:09:03 +0100

    Refactor employee vcard handling.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,16 +1,17 @@
 # encoding: utf-8
 
 class Employee < ActiveRecord::Base
-  belongs_to :work_vcard, :class_name => 'Vcard', :foreign_key => :work_vcard_id
-  belongs_to :private_vcard, :class_name => 'Vcard', :foreign_key => :private_vcard_id
+  # Access restrictions
+  attr_accessible :code, :born_on, :workload
 
-  # Proxies
+  # TODO support multiple vcards
+  has_vcards
   has_one :vcard, :as => :object
   accepts_nested_attributes_for :vcard
   attr_accessible :vcard, :vcard_attributes
 
   def name
-    (work_vcard || private_vcard).try(:full_name)
+    vcard.try(:full_name)
   end
 
   def to_s
