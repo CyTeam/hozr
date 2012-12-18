ce89a2413c486ac4ca7fd3e9d8a69cd19dd0fb5d
Andi Staub (Cyt GmbH) <andi.staub@cyt.ch>
2012-12-18 15:23:39 +0100

    Simplify CaseNr logic.

--- app/models/case_nr.rb
+++ app/models/case_nr.rb
@@ -2,38 +2,30 @@
 
 class CaseNr
   attr_accessor :year, :nr
-  
+
   def initialize(*params)
     @delimiter='/'
-    
+
     case params.size
     when 0
-      max_case_nr = Case.maximum(:praxistar_eingangsnr, :conditions => "praxistar_eingangsnr < '90/'")
-      if max_case_nr.nil? or max_case_nr == ''
-        parse("1")
-      else
-        parse(max_case_nr)
-        inc!
-      end
+      free
     when 1
-      param = params[0]
-      case param.class.name
-      when 'String'
-        parse(param)
-      when 'Fixnum'
-        parse(param.to_s)
-      when 'Hash'
-        @year = param[:year]
-        @nr = param[:nr]
-      end
-    when 2
-      @year = params[0].to_i
-      @nr = params[1].to_i
+      parse(params[0])
     end
   end
-  
+
+  def free
+    max_case_nr = Case.maximum(:praxistar_eingangsnr, :conditions => "praxistar_eingangsnr < '90/'")
+    if max_case_nr.blank?
+      parse("1")
+    else
+      parse(max_case_nr)
+      inc!
+    end
+  end
+
   def parse(value)
-    left, right = value.split(/[\/-]/)
+    left, right = value.to_s.split(/[\/-]/)
     if right.nil?
       @year = Date.today.strftime("%y").to_i
       @nr = left.to_i
@@ -48,29 +40,8 @@ class CaseNr
     return "#{sprintf("%02d", @year)}#{@delimiter}#{sprintf("%05d", @nr)}"
   end
 
-  def inc
-    clone.inc!
-  end
-
   def inc!
     @nr += 1
     self
-  end
-  
-  def dec!
-    @nr -= 1
-    self
-  end
-
-  def dec
-    clone.dec!
-  end
-  
-  def self.free
-    new
-  end
-
-  def self.next(previous = nil)
-    previous ? new(previous).inc! : new
   end
 end
