740793145decd0cb0c43b843edddf5161824104c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-03 13:56:08 +0100

    Use Person instead of Employee as object_type in user_object_collection.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,7 +2,7 @@ module UserHelper
   def user_object_collection
     [
       [t_model(Doctor), Doctor.all.map{|u| [u.to_s, "Doctor:#{u.id}"]}],
-      [t_model(Employee), Employee.all.map{|u| [u.to_s, "Employee:#{u.id}"]}]
+      [t_model(Employee), Employee.all.map{|u| [u.to_s, "Person:#{u.id}"]}]
     ]
   end
 end
