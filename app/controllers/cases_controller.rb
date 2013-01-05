34dbbcc5d7765ba069345fe433494c76d56d0cb6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-05 21:43:51 +0100

    Drop authorize_resource call in cases controller.

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8'
 
 class CasesController < AuthorizedController
-  authorize_resource
-
   # Auto Completion
   auto_complete_for :finding_class, :selection, :limit => 12
   autocomplete :finding_class, [:code, :name], :column => '', :extra_data => [:name], :display_value => :to_s, :full => true
