fb368d92f8a1a9f9d3a23daff83b2a6b2806cdb5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-05 21:43:19 +0100

    Drop helper declaration in cases controller.

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -3,10 +3,6 @@
 class CasesController < AuthorizedController
   authorize_resource
 
-  # Helpers
-  # =======
-  helper :doctors
-
   # Auto Completion
   auto_complete_for :finding_class, :selection, :limit => 12
   autocomplete :finding_class, [:code, :name], :column => '', :extra_data => [:name], :display_value => :to_s, :full => true
