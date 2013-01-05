4e86a26c519290d11f5f42b6717add4f28c77f4f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-05 13:48:55 +0100

    Fix trailing whitespaces in patients controller.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -7,7 +7,7 @@ class PatientsController < AuthorizedController
   def dunning_stopped
     @patients = Patient.dunning_stopped.all
   end
-  
+
   def show_history
     @patient = Patient.find(params[:id])
     @cases = @patient.cases
@@ -15,13 +15,13 @@ class PatientsController < AuthorizedController
 
   def new
     @patient = Patient.new(params[:patient])
-    
+
     @patient.vcard.honorific_prefix = 'Frau'
   end
 
   def create
     @patient = Patient.new(params[:patient])
-    
+
     # Deduce sex from honorific_prefix
     @patient.sex = HonorificPrefix.find_by_name(@patient.vcard.honorific_prefix).sex
 
@@ -30,7 +30,7 @@ class PatientsController < AuthorizedController
 
   def update
     @patient = Patient.find(params[:id])
-    
+
     # Deduce sex from honorific_prefix
     @patient.sex = HonorificPrefix.find_by_name(@patient.vcard.honorific_prefix).sex
 
