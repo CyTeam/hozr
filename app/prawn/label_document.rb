1494356f0ed12260fedcb2df771a2e6286302e6e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-08 13:07:37 +0100

    Add LabelDocument.

---
+++ app/prawn/label_document.rb
@@ -0,0 +1,56 @@
+require 'prawn/measurement_extensions'
+
+# Barcode
+require 'barby'
+require 'barby/barcode/code_128'
+require 'barby/outputter/prawn_outputter'
+
+class LabelDocument < Prawn::Document
+  def default_options
+    {
+      :page_size => [12.mm, 23.mm],
+      :top_margin => 0.mm,
+      :bottom_margin => 0.mm,
+      :left_margin => 0.mm,
+      :right_margin => 3.mm
+    }
+  end
+
+  def label(date, day_nr, part_nr)
+    enc_date = date.strftime('%y%j')
+    code = '%s%03i%02i' % [enc_date, day_nr, part_nr]
+    text_code = '%s %03i/%02i' % [date.strftime('%d.%m.%y'), day_nr, part_nr]
+
+    barcode = Barby::Code128C.new(code)
+    rotate(90, :origin => [0,0]) do
+      barcode.annotate_pdf(self, :unbleed => 0.1, :height => 35, :xdim => 0.6, :y => -20, :x => 4)
+
+      font_size 6
+      draw_text text_code, :at => [10, -26]
+    end
+  end
+
+  def to_pdf(date, day_nr, part_count)
+    (1..part_count).each do |i|
+      label(date, day_nr, i)
+      start_new_page unless i == part_count
+    end
+
+    render
+  end
+
+  def initialize
+    super default_options
+  end
+
+  def print(printer, *args)
+    # Workaround TransientJob not yet accepting options
+    file = Tempfile.new('')
+    file.binmode
+    file.puts(to_pdf(*args))
+    file.close
+
+    printer.print_file(file.path)
+  end
+end
+
