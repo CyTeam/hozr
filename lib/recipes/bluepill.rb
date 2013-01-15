4b283e60af1d40e7175e905efe99d35ee1b8bc88
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-15 14:00:08 +0100

    Add bluepill:configure and bluepill:load cap tasks.

--- lib/recipes/bluepill.rb
+++ lib/recipes/bluepill.rb
@@ -15,7 +15,24 @@ namespace :deploy do
   end
 end
 
+before "deploy:setup", "bluepill:configure"
+
 namespace :bluepill do
+  desc "Create bluepill recipe"
+  task :configure, :roles => [:app], :except => { :no_release => true } do
+    #{shared_path}
+    location = fetch(:template_dir, "config/deploy") + '/bluepill.rb.erb'
+    template = File.read(location)
+    config = ERB.new(template)
+
+    put config.result(binding), "#{shared_path}/config/bluepill.rb"
+  end
+
+  desc "Load bluepill recipe"
+  task :load, :roles => [:app], :except => { :no_release => true } do
+    run "bluepill --no-privileged load #{shared_path}/config/bluepill.rb"
+  end
+
   desc "Prints bluepills monitored processes statuses"
   task :status, :roles => [:app] do
     set :bluepill_app, "#{client}.#{domain}"
