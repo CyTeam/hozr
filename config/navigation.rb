9393861f7afaffa80090ed428bd1fe35f6af2b7e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 18:34:21 +0100

    Set simple_navigation active class to match bootstrap.

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.selected_class = 'active'
+
   navigation.items do |primary|
     primary.item :admin_work_queue, 'TODO', admin_work_queue_path
     primary.item :entry, 'Eingang', '#' do |entry|
