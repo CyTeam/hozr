02f0063f988777b600385d57b19873ed89e89219
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-28 12:13:35 +0100

    Adapt search to directly jump to case if query is matching case code.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,16 @@ class SearchController < ApplicationController
   def index
     query = params[:query]
 
-    @patients = Patient.by_text(query, :star => true, :per_page => 30, :page => params[:page])
+    case query
+    when /B?([0-9]{8})/
+      # CaseCode
+      code = $1
+      cases = Case.where(:praxistar_eingangsnr => code)
+      if cases.exists?
+        redirect_to cases.first
+      end
+    end
+
+    @collection = Patient.by_text(query, :star => true, :per_page => 30, :page => params[:page])
   end
 end
