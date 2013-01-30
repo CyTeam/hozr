7b4a144aa2fe0bc098513a03cb632e68aa54fddb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-30 11:56:44 +0100

    Send BCC to sender for email results.

--- app/mailers/case_mailer.rb
+++ app/mailers/case_mailer.rb
@@ -7,8 +7,10 @@ class CaseMailer < ActionMailer::Base
     prawn_options = { :page_size => 'A4' }
     attachments[a_case.pdf_name] = ResultReport.new(prawn_options).to_pdf(a_case)
 
+    # TODO: respect mail.result_report.bcc setting
     mail(
       :to => a_case.doctor.email,
+      :bcc => @case.screener.tenant.person.vcard.contacts.by_type('E-Mail').first,
       :from => @case.screener.tenant.person.vcard.contacts.by_type('E-Mail').first,
       :subject => "Resultat #{a_case.to_s}"
     )
