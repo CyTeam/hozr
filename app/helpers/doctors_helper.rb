833509d68769d4bd2a8eb152e056bb26d69f73fa
Simon Hürlimann <simon.huerlimann@cyt.ch>
2013-03-14 11:02:43 +0100

    Use Doctor.to_s(:select) for doctors_collection helper.

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8'
 module DoctorsHelper
   def doctors_collection
-    Doctor.active.includes(:vcards => :address).order('vcards.family_name, vcards.given_name').collect { |m| [ m.to_s, m.id ] }
+    Doctor.active.includes(:vcards => :address).order('vcards.family_name, vcards.given_name').collect { |m| [ m.to_s(:select), m.id ] }
   end
 
   def doctor_channels_collection
