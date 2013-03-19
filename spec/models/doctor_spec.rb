2bb7bb407eedeeacf1ae3784af1f0df367489cce
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-03-19 10:33:34 +0100

    Add Doctor#channels specs.

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,4 +1,32 @@
 require "spec_helper"
 
 describe Doctor do
+  context "#channels" do
+    let(:doctor) { Doctor.new }
+    it "should return empty array if result_report_channels config empty" do
+      doctor.stub(:settings => {:result_report_channels => ""})
+      doctor.channels.should == []
+    end
+
+    it "should return empty array if result_report_channels config nil" do
+      doctor.stub(:settings => {:result_report_channels => nil})
+      doctor.channels.should == []
+    end
+
+    it "should return no nil channels" do
+      doctor.stub(:settings => {:result_report_channels => [nil, 'print', ''].to_yaml})
+      doctor.channels.should_not include(nil)
+    end
+
+    it "should return no empty channels" do
+      doctor.stub(:settings => {:result_report_channels => [nil, 'print', ''].to_yaml})
+      doctor.channels.should_not include('')
+    end
+
+    it "should return all channels" do
+      doctor.stub(:settings => {:result_report_channels => [nil, 'print', '', 'email'].to_yaml})
+      doctor.channels.should include('print')
+      doctor.channels.should include('email')
+    end
+  end
 end
