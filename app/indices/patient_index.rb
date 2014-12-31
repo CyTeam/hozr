d7eedd82aa9c7ad1f924afeafc502531f98eced0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-12-31 18:37:11 +0100

    Add aliases to billing_vcard fields in patient search index.

--- app/indices/patient_index.rb
+++ app/indices/patient_index.rb
@@ -12,16 +12,16 @@ ThinkingSphinx::Index.define :patient, :with => :active_record, :delta => true d
   indexes vcard.address.extended_address
   indexes vcard.address.post_office_box
 
-  indexes billing_vcard.full_name
-  indexes billing_vcard.nickname
-  indexes billing_vcard.family_name
-  indexes billing_vcard.given_name
-  indexes billing_vcard.additional_name
-  indexes billing_vcard.address.street_address
-  indexes billing_vcard.address.postal_code
-  indexes billing_vcard.address.locality
-  indexes billing_vcard.address.extended_address
-  indexes billing_vcard.address.post_office_box
+  indexes billing_vcard.full_name, :as => :billing_full_name
+  indexes billing_vcard.nickname, :as => :billing_nickname
+  indexes billing_vcard.family_name, :as => :billing_family_name
+  indexes billing_vcard.given_name, :as => :billing_given_name
+  indexes billing_vcard.additional_name, :as => :billing_additional_name
+  indexes billing_vcard.address.street_address, :as => :billing_street_address
+  indexes billing_vcard.address.postal_code, :as => :billing_postal_code
+  indexes billing_vcard.address.locality, :as => :billing_locality
+  indexes billing_vcard.address.extended_address, :as => :billing_extended_address
+  indexes billing_vcard.address.post_office_box, :as => :billing_post_office_box
 
   indexes doctor_patient_nr
 
