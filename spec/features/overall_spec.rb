45147ee23b752ef6775a76f0d0595105eecb0fcb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-12-31 12:32:53 +0100

    Add login feature spec.

--- spec/features/overall_spec.rb
+++ spec/features/overall_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 feature 'overall functionality' do
+  let!(:user_admin) { FactoryGirl.create :user_admin }
+
   scenario 'I can see the login page', js: true do
+    # Login
     visit root_path
+
+    fill_in "Benutzername", :with => user_admin.login
+    fill_in "Passwort", :with => user_admin.password
+    click_button "Anmelden"
+
+    expect(page).to have_text('Auftragsformulare')
   end
 end
